5d6be6961d3f87204f8ce126dbe10fd08d3ca2dd
nwalsh <nwalsh@62cac252-8da6-4816-9e9d-6dc37b19578c>
2012-03-26 21:04:20 +0000

    Added support for OrQuery, NotQuery, NearQuery, CollectionQuery, and DirectoryQuery
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/trunk@100057 62cac252-8da6-4816-9e9d-6dc37b19578c

--- src/main/java/com/marklogic/client/config/search/StructuredQueryBuilder.java
+++ src/main/java/com/marklogic/client/config/search/StructuredQueryBuilder.java
@@ -11,12 +11,28 @@ package com.marklogic.client.config.search;
 // TODO: Implement the rest of the query types
 
 public class StructuredQueryBuilder {
-    String optionsName = null;
+    private String optionsName = null;
 
+    public enum Ordering {
+        ORDERED, UNORDERED;
+    }
+    
+    public enum Operator {
+        LT, LE, GT, GE, EQ, NE
+    }
+    
     public StructuredQueryBuilder(String optionsName) {
         this.optionsName = optionsName;
     }
-    
+
+    public OrQuery or(StructuredQueryDefinition... queries) {
+        return new OrQuery(queries);
+    }
+
+    public NotQuery not(StructuredQueryDefinition query) {
+        return new NotQuery(query);
+    }
+
     public AndQuery and(StructuredQueryDefinition... queries) {
         return new AndQuery(queries);
     }
@@ -32,7 +48,38 @@ public class StructuredQueryBuilder {
     public TermQuery term(String term) {
         return new TermQuery(term);
     }
-    
+
+    public NearQuery near(StructuredQueryDefinition... queries) {
+        return new NearQuery(queries);
+    }
+
+    public NearQuery near(int distance, double weight, Ordering order, StructuredQueryDefinition... queries) {
+        return new NearQuery(distance, weight, order, queries);
+    }
+
+    public CollectionQuery collection(String... uris) {
+        return new CollectionQuery(uris);
+    }
+
+    public DirectoryQuery directory(boolean isInfinite, String... uris) {
+        return new DirectoryQuery(isInfinite, uris);
+    }
+
+    /*
+    public DocumentFragmentQuery documentFragment(StructuredQueryDefinition... queries) { ... }
+    public PropertiesQuery properties(StructuredQueryDefinition... queries) { ... }
+    public LocksQuery locks(StructuredQueryDefinition... queries) { ... }
+    public ElementConstraintQuery elementConstraint(String constraintName, StructuredQueryDefinition... queries) { ... }
+    public PropertiesConstraintQuery propertiesConstraint(String constraintName, StructuredQueryDefinition... queries) { ... }
+    public CollectionConstraintQuery collectionConstraint(String constraintName, String uri) { ... }
+    public ValueConstraintQuery valueConstraint(String constraintName, String value, double weight) { ... }
+    public WordConstraintQuery wordConstraint(String constraintName, String word, double weight) { ... }
+    public RangeConstraintQuery rangeConstraint(String constraintName, Operator operator, String value) { ... }
+    public GeospatialConstraintQuery geospatialConstraint(String constraintName, Region... regions) { ... }
+    */
+
+    /* ************************************************************************************* */
+
     private abstract class AbstractStructuredQuery implements StructuredQueryDefinition {
         String uri = null;
 
@@ -67,6 +114,34 @@ public class StructuredQueryBuilder {
                 s += q.serialize();
             }
             return s + "</and-query>";
+        }
+    }
+
+    public class OrQuery extends AbstractStructuredQuery {
+        StructuredQueryDefinition queries[] = null;
+
+        public OrQuery(StructuredQueryDefinition... queries) {
+            this.queries = queries;
+        }
+
+        public String innerSerialize() {
+            String s = "<or-query>";
+            for (StructuredQueryDefinition q : queries) {
+                s += q.serialize();
+            }
+            return s + "</or-query>";
+        }
+    }
+
+    public class NotQuery extends AbstractStructuredQuery {
+        StructuredQueryDefinition query = null;
+
+        public NotQuery(StructuredQueryDefinition query) {
+            this.query = query;
+        }
+
+        public String innerSerialize() {
+            return "<not-query>" + query.serialize() + "</not-query>";
         }
     }
 
@@ -105,6 +180,74 @@ public class StructuredQueryBuilder {
 
         public String innerSerialize() {
             return "<term><text>" + term + "</text></term>";
+        }
+    }
+
+    public class NearQuery extends AbstractStructuredQuery {
+        private int distance = 10;
+        private double weight = 1.0;
+        private Ordering order = Ordering.UNORDERED;
+        private StructuredQueryDefinition queries[] = null;
+
+        public NearQuery(StructuredQueryDefinition... queries) {
+            this.queries = queries;
+        }
+
+        public NearQuery(int distance, double weight, Ordering order, StructuredQueryDefinition... queries) {
+            this.distance = distance;
+            this.weight = weight;
+            this.order = order;
+            this.queries = queries;
+        }
+
+        public String innerSerialize() {
+            String s = "<near-query>";
+            for (StructuredQueryDefinition q : queries) {
+                s += q.serialize();
+            }
+            s += "<ordered>" + (order == Ordering.ORDERED) + "</ordered>";
+            s += "<distance>" + distance + "</distance>";
+            s += "<distance-weight>" + weight + "</distance-weight>";
+            return s + "</near-query>";
+        }
+    }
+
+    public class CollectionQuery extends AbstractStructuredQuery {
+        private String uris[] = null;
+
+        public CollectionQuery(String... uris) {
+            this.uris = uris;
+        }
+
+        public String innerSerialize() {
+            String s = "<collection-query>";
+            if (uris != null) {
+                for (String uri : uris) {
+                    s += "<uri>" + uri + "</uri>";
+                }
+            }
+            return s + "</collection-query>";
+        }
+    }
+
+    public class DirectoryQuery extends AbstractStructuredQuery {
+        private String uris[] = null;
+        private boolean infinite = false;
+
+        public DirectoryQuery(boolean isInfinite, String... uris) {
+            infinite = isInfinite;
+            this.uris = uris;
+        }
+
+        public String innerSerialize() {
+            String s = "<directory-query>";
+            if (uris != null) {
+                for (String uri : uris) {
+                    s += "<uri>" + uri + "</uri>";
+                }
+            }
+            s += "<infinite>" + infinite + "</infinite>";
+            return s + "</directory-query>";
         }
     }
 }
