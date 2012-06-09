dd5c4ec32fcd1b9abe37643442a437a1d7fc6575
ehennum <ehennum@62cac252-8da6-4816-9e9d-6dc37b19578c>
2012-06-09 16:25:35 +0000

    package JavaDoc for examples
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/trunk@106255 62cac252-8da6-4816-9e9d-6dc37b19578c

---
+++ src/main/java/com/marklogic/client/example/handle/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * The package provides examples of extensions that read or write database documents
+ * in a new way or with a new representation.
+ * For instance, {@link com.marklogic.client.example.handle.JDOMHandle} writes
+ * XML documents using the Open Source JDOM library.
+ */
+/*
+ * Copyright 2012 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.client.example.handle;
