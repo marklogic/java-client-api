51a1a4bc5c50239e29fec2085b8d2c455f75c81d
ehennum <ehennum@62cac252-8da6-4816-9e9d-6dc37b19578c>
2012-07-04 22:36:19 +0000

    JavaDoc
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/trunk@108345 62cac252-8da6-4816-9e9d-6dc37b19578c

--- src/main/java/com/marklogic/client/DatabaseClient.java
+++ src/main/java/com/marklogic/client/DatabaseClient.java
@@ -30,33 +30,90 @@ public interface DatabaseClient {
      * 
      * To call openTransaction(), an application must authenticate as rest-writer or rest-admin.
      * 
-     * @return
-     * @throws ForbiddenUserException
-     * @throws FailedRequestException
+     * @return	a Transaction object identifying and supporting operations on the transaction
      */
 	public Transaction openTransaction() throws ForbiddenUserException, FailedRequestException;
-
+	/**
+	 * Starts a transaction with the specified name, which makes the transaction easier to recognize
+	 * when you get status reports.
+	 * 
+	 * @param name	the transaction name
+     * @return	a Transaction object identifying and supporting operations on the transaction
+	 */
 	public Transaction openTransaction(String name) throws ForbiddenUserException, FailedRequestException;
-
+	/**
+	 * Starts a transaction with the specified name and time limit. If the transaction is not committed
+	 * or rolled back within the specified time, the transaction rolls back automatically.
+	 * 
+	 * @param name	the transaction name
+	 * @param timeLimit	the number of the transaction in seconds
+     * @return	a Transaction object identifying and supporting operations on the transaction
+	 */
 	public Transaction openTransaction(String name, int timeLimit) throws ForbiddenUserException, FailedRequestException;
 
-    // factory methods for document managers
+    /**
+     * Creates a document manager for documents with unknown or heterogenous formats.
+     * @return	a manager supporting generic operations on documents
+     */
     public GenericDocumentManager newDocumentManager();
+    /**
+     * Creates a document manager for documents with a binary format such as images.
+     * @return	a manager supporting operations on binary documents
+     */
     public BinaryDocumentManager  newBinaryDocumentManager();
+    /**
+     * Creates a document manager for documents containing a JSON structure.
+     * @return	a manager supporting operations on JSON documents
+     */
     public JSONDocumentManager    newJSONDocumentManager();
+    /**
+     * Creates a document manager for documents containing unstructured text.
+     * @return	a manager supporting operations on text documents
+     */
     public TextDocumentManager    newTextDocumentManager();
+    /**
+     * Creates a document manager for documents containing XML.
+     * @return	a manager supporting operations on XMLdocuments
+     */
     public XMLDocumentManager     newXMLDocumentManager();
- 
-    public RequestLogger newLogger(OutputStream out);
- 
+
+    /**
+     * Creates a manager for querying the database.
+     * @return	a manager supporting search operations and lookup of values and tuples in indexes (also known as lexicons)
+     */
     public QueryManager newQueryManager();
 
+    /**
+     * Creates a manager for configuring the REST server for the database. The
+     * ServerConfigurationManager can persist query options and transforms or
+     * set properties of the server. The application must have rest-admin
+     * privilieges to use the ServerConfigurationManager.
+     * 
+     * @return	a manager for the server properties or administrative resources
+     */
     public ServerConfigurationManager newServerConfigManager();
 
+    /**
+     * Initializes a manager for a extension resource.
+     * 
+     * @param resourceName	the name of the extension resource
+     * @param resourceManager	the manager for the extension resource
+     * @return	the initialized resource manager
+     */
     public <T extends ResourceManager> T init(String resourceName, T resourceManager);
 
     /**
-     * Closes a database connection.  After the connection is closed,
+     * Creates a logger for document and query requests.  To merge the logging output
+     * with the output from other loggers, pass the output stream used by the other
+     * loggers.
+     * 
+     * @param out	the output stream for the logging output
+     * @return	the logger for client requests
+     */
+    public RequestLogger newLogger(OutputStream out);
+
+    /**
+     * Closes the database client and releases associated resources.  After the client is closed,
      * document and query managers can no longer access the database.
      */
     public void release();
