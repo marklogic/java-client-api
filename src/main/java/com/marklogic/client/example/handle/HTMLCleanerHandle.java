bf48ca3a483dceb77201492e6938e2db33641e90
ehennum <ehennum@62cac252-8da6-4816-9e9d-6dc37b19578c>
2012-07-13 15:59:26 +0000

    handle example for client-side HTML cleaning
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/trunk@109246 62cac252-8da6-4816-9e9d-6dc37b19578c

---
+++ src/main/java/com/marklogic/client/example/handle/HTMLCleanerHandle.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2012 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.client.example.handle;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.CompactXmlSerializer;
+import org.htmlcleaner.DefaultTagProvider;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.ITagInfoProvider;
+import org.htmlcleaner.TagNode;
+import org.htmlcleaner.XmlSerializer;
+
+import com.marklogic.client.Format;
+import com.marklogic.client.io.BaseHandle;
+import com.marklogic.client.io.OutputStreamSender;
+import com.marklogic.client.io.marker.OperationNotSupported;
+import com.marklogic.client.io.marker.XMLWriteHandle;
+
+/**
+ * An HTMLCleaner Handle writes an HTML document to the database
+ * as an XHTML document.
+ */
+public class HTMLCleanerHandle
+	extends BaseHandle<OperationNotSupported, OutputStreamSender>
+	implements OutputStreamSender, XMLWriteHandle
+{
+	private CleanerProperties configuration;
+	private ITagInfoProvider  rulesProvider;
+	private HtmlCleaner       parser;
+	private XmlSerializer     serializer;
+	private TagNode           content;
+
+	public HTMLCleanerHandle() {
+		super();
+		super.setFormat(Format.XML);
+	}
+	public HTMLCleanerHandle(TagNode content) {
+		this();
+    	set(content);
+	}
+
+	public CleanerProperties getConfiguration() {
+		if (configuration == null)
+			configuration = makeConfiguration();
+		return configuration;
+	}
+	public void setConfiguration(CleanerProperties configuration) {
+		this.configuration = configuration;
+	}
+	protected CleanerProperties makeConfiguration() {
+		return new CleanerProperties();
+	}
+
+	public ITagInfoProvider getRulesProvider() {
+		if (rulesProvider == null)
+			rulesProvider = makeRulesProvider();
+		return rulesProvider;
+	}
+	public void setRulesProvider(ITagInfoProvider rulesProvider) {
+		this.rulesProvider = rulesProvider;
+	}
+	protected ITagInfoProvider makeRulesProvider() {
+		return new DefaultTagProvider();
+	}
+
+	public HtmlCleaner getParser() {
+		if (parser == null)
+			parser = makeParser();
+		return parser;
+	}
+	public void setParser(HtmlCleaner parser) {
+		this.parser = parser;
+	}
+	protected HtmlCleaner makeParser() {
+		return new HtmlCleaner(getRulesProvider(), getConfiguration());
+	}
+
+	public XmlSerializer getSerializer() {
+		if (serializer == null)
+			serializer = makeSerializer();
+		return serializer;
+	}
+	public void setSerializer(XmlSerializer serializer) {
+		this.serializer = serializer;
+	}
+	protected XmlSerializer makeSerializer() {
+		CleanerProperties configuration = getConfiguration();
+		return new CompactXmlSerializer(
+				(configuration != null) ?
+						configuration : getParser().getProperties()
+				);
+	}
+
+	public TagNode get() {
+		return content;
+	}
+    public void set(TagNode content) {
+    	this.content = content;
+    }
+    public HTMLCleanerHandle with(TagNode content) {
+    	set(content);
+    	return this;
+    }
+
+    public void set(File content, String charset) throws IOException {
+    	set(getParser().clean(content, charset));
+    }
+    public HTMLCleanerHandle with(File content, String charset)
+    throws IOException {
+    	set(content, charset);
+    	return this;
+    }
+
+    public void set(InputStream content, String charset) throws IOException {
+    	set(getParser().clean(content, charset));
+    }
+    public HTMLCleanerHandle with(InputStream content, String charset)
+    throws IOException {
+    	set(content, charset);
+    	return this;
+    }
+
+    public void set(Reader content) throws IOException {
+    	set(getParser().clean(content));
+    }
+    public HTMLCleanerHandle with(Reader content) throws IOException {
+    	set(content);
+    	return this;
+    }
+
+    public void set(String content) throws IOException {
+    	set(getParser().clean(content));
+    }
+    public HTMLCleanerHandle with(String content) throws IOException {
+    	set(content);
+    	return this;
+    }
+
+	public void setFormat(Format format) {
+		if (format != Format.XML)
+			throw new IllegalArgumentException(
+					"HTMLCleanerHandle supports the XML format only");
+	}
+
+	@Override
+	protected OutputStreamSender sendContent() {
+		if (content == null) {
+			throw new IllegalStateException("No content to write");
+		}
+
+		return this;
+	}
+	@Override
+	public void write(OutputStream out) throws IOException {
+		getSerializer().writeToStream(content, out, "UTF-8");
+	}
+
+}
---
+++ src/main/java/com/marklogic/client/example/handle/HTMLCleanerHandleExample.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2012 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.client.example.handle;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.ITagInfoProvider;
+import org.htmlcleaner.TagInfo;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.DatabaseClientFactory.Authentication;
+import com.marklogic.client.XMLDocumentManager;
+import com.marklogic.client.io.StringHandle;
+
+/**
+ * HTMLCleanerHandleExample illustrates writing HTML content as
+ * an indexable XHTML document using the HTMLCleanerHandle example
+ * of a content handle extension.
+ */
+public class HTMLCleanerHandleExample {
+
+	public static void main(String[] args) throws IOException {
+		Properties props = loadProperties();
+
+		// connection parameters for writer user
+		String         host            = props.getProperty("example.host");
+		int            port            = Integer.parseInt(props.getProperty("example.port"));
+		String         writer_user     = props.getProperty("example.writer_user");
+		String         writer_password = props.getProperty("example.writer_password");
+		Authentication authType        = Authentication.valueOf(
+				props.getProperty("example.authentication_type").toUpperCase()
+				);
+
+		run(host, port, writer_user, writer_password, authType);
+	}
+
+	public static void run(String host, int port, String user, String password, Authentication authType)
+	throws IOException {
+		System.out.println("example: "+HTMLCleanerHandleExample.class.getName());
+
+		String fileroot = "sentiment";
+
+		// create the client
+		DatabaseClient client = DatabaseClientFactory.newClient(host, port, user, password, authType);
+
+		// create a manager for documents of any format
+		XMLDocumentManager docMgr = client.newXMLDocumentManager();
+
+		// read the example file
+		InputStream docStream = HTMLCleanerHandleExample.class.getClassLoader().getResourceAsStream(
+				"data"+File.separator+fileroot+".html");
+		if (docStream == null)
+			throw new RuntimeException("Could not read document example");
+
+		// create an identifier for the document
+		String docId = "/example/"+fileroot+".xhtml";
+
+		// create a handle for the content
+		HTMLCleanerHandle writeHandle = new HTMLCleanerHandle();
+
+		// configure the parser rules for this content
+		ITagInfoProvider rules = writeHandle.getRulesProvider();
+		TagInfo rule = rules.getTagInfo("p");
+		Set pCloseTags = rule.getMustCloseTags();
+		pCloseTags.add("h1");
+
+		// set the handle to the parsed HTML content
+		writeHandle.set(docStream, "UTF-8");
+
+		// write the converted XHTML content
+		docMgr.write(docId, writeHandle);
+
+		// create a handle to receive the XHTML content
+		StringHandle readHandle = new StringHandle();
+
+		// read the document content
+		docMgr.read(docId, readHandle);
+
+		// delete the document
+		docMgr.delete(docId);
+
+		System.out.println("Wrote /example/"+fileroot+".xhtml using HTMLCleaner\n"+
+				readHandle.get());
+
+		// release the client
+		client.release();
+	}
+
+	// get the configuration for the example
+	public static Properties loadProperties() throws IOException {
+		String propsName = "Example.properties";
+		InputStream propsStream = HTMLCleanerHandleExample.class.getClassLoader().getResourceAsStream(propsName);
+		if (propsStream == null)
+			throw new RuntimeException("Could not read example properties");
+
+		Properties props = new Properties();
+		props.load(propsStream);
+
+		return props;
+	}
+}
