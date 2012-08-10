b44dabd49626645af7e627fc0e9e0b347bab6a22
cgreer <cgreer@62cac252-8da6-4816-9e9d-6dc37b19578c>
2012-08-10 22:30:27 +0000

    bug:18846 test cases for namespaces in keyvalue
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/trunk@111954 62cac252-8da6-4816-9e9d-6dc37b19578c

--- src/test/java/com/marklogic/client/test/KeyValueSearchTest.java
+++ src/test/java/com/marklogic/client/test/KeyValueSearchTest.java
@@ -15,71 +15,139 @@
  */
 package com.marklogic.client.test;
 
-import com.marklogic.client.query.QueryManager;
-import com.marklogic.client.query.KeyValueQueryDefinition;
-import com.marklogic.client.query.MatchDocumentSummary;
-import com.marklogic.client.query.MatchLocation;
-import com.marklogic.client.io.SearchHandle;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import javax.xml.namespace.QName;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.xml.namespace.QName;
-import java.io.IOException;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import com.marklogic.client.FailedRequestException;
+import com.marklogic.client.admin.NamespacesManager;
+import com.marklogic.client.io.SearchHandle;
+import com.marklogic.client.query.KeyValueQueryDefinition;
+import com.marklogic.client.query.MatchDocumentSummary;
+import com.marklogic.client.query.MatchLocation;
+import com.marklogic.client.query.QueryManager;
 
 public class KeyValueSearchTest {
-    @BeforeClass
-    public static void beforeClass() {
-        Common.connect();
-    }
+	@BeforeClass
+	public static void beforeClass() {
+		Common.connectAdmin();
 
-    @AfterClass
-    public static void afterClass() {
-        Common.release();
-    }
+		// setup namespaces to test kv with namespaces
+		NamespacesManager nsMgr = Common.client.newServerConfigManager()
+				.newNamespacesManager();
 
-    @Test
-    public void testKVSearch() throws IOException {
-        QueryManager queryMgr = Common.client.newQueryManager();
-        KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+		nsMgr.updatePrefix("ns1", "http://marklogic.com/test-ns1");
+		nsMgr.updatePrefix("ns2", "http://marklogic.com/test-ns2");
 
-        qdef.put(queryMgr.newElementLocator(new QName("leaf")), "leaf3");
-        SearchHandle results = queryMgr.search(qdef, new SearchHandle());
-        assertNotNull(results);
-        assertFalse(results.getMetrics().getTotalTime() == -1);
+		Common.release();
+		Common.connect();
+	}
 
-        MatchDocumentSummary[] summaries = results.getMatchResults();
-        for (MatchDocumentSummary summary : summaries) {
-            MatchLocation[] locations = summary.getMatchLocations();
-            for (MatchLocation location : locations) {
-                assertNotNull(location.getAllSnippetText());
-            }
-        }
-        
-        assertNotNull(summaries);
-    }
+	@AfterClass
+	public static void afterClass() {
+		Common.release();
+		Common.connectAdmin();
+		NamespacesManager nsMgr = Common.client.newServerConfigManager()
+				.newNamespacesManager();
 
-    @Test
-    public void testJsonSearch() throws IOException {
-        QueryManager queryMgr = Common.client.newQueryManager();
-        KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+		nsMgr.deleteAll();
 
-        qdef.put(queryMgr.newKeyLocator("firstKey"), "first value");
-        SearchHandle results = queryMgr.search(qdef, new SearchHandle());
-        assertNotNull(results);
-        assertFalse(results.getMetrics().getTotalTime() == -1);
+		Common.release();
 
-        MatchDocumentSummary[] summaries = results.getMatchResults();
-        for (MatchDocumentSummary summary : summaries) {
-            MatchLocation[] locations = summary.getMatchLocations();
-            for (MatchLocation location : locations) {
-                assertNotNull(location.getAllSnippetText());
-            }
-        }
+	}
 
-        assertNotNull(summaries);
-    }
+	@Test
+	public void testKVSearch() throws IOException {
+		QueryManager queryMgr = Common.client.newQueryManager();
+		KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+
+		qdef.put(queryMgr.newElementLocator(new QName("leaf")), "leaf3");
+		SearchHandle results = queryMgr.search(qdef, new SearchHandle());
+		assertNotNull(results);
+		assertFalse(results.getMetrics().getTotalTime() == -1);
+
+		MatchDocumentSummary[] summaries = results.getMatchResults();
+		for (MatchDocumentSummary summary : summaries) {
+			MatchLocation[] locations = summary.getMatchLocations();
+			for (MatchLocation location : locations) {
+				assertNotNull(location.getAllSnippetText());
+			}
+		}
+
+		assertNotNull(summaries);
+	}
+
+	@Test(expected=FailedRequestException.class)
+	public void testKVSearchBadNamespacePrefix() throws IOException {
+		QueryManager queryMgr = Common.client.newQueryManager();
+		KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+
+		qdef.put(queryMgr.newElementLocator(new QName("badprefix:leaf")),
+				"leaf3");
+		@SuppressWarnings("unused")
+		SearchHandle results = queryMgr.search(qdef, new SearchHandle());
+		fail("Test should have thrown a Failed Request with bad prefix");
+	}
+	
+	@Test(expected=FailedRequestException.class)
+	public void testKVSearchInadequateQName() throws IOException {
+		QueryManager queryMgr = Common.client.newQueryManager();
+		KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+
+		qdef.put(queryMgr.newElementLocator(new QName("http://marklogic.com/test-namespace-with-no-prefix", "leaf")),
+				"leaf3");
+		@SuppressWarnings("unused")
+		SearchHandle results = queryMgr.search(qdef, new SearchHandle());
+		fail("Test should have thrown a Failed Request with bad prefix");
+	}
+
+	@Test
+	public void testKVSearchGoodNamespacePrefix() throws IOException {
+		QueryManager queryMgr = Common.client.newQueryManager();
+		KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+
+		qdef.put(queryMgr.newElementLocator(new QName("ns1:leaf")), "leaf3");
+		SearchHandle results = queryMgr.search(qdef, new SearchHandle());
+		assertNotNull(results);
+		assertFalse(results.getMetrics().getTotalTime() == -1);
+
+		MatchDocumentSummary[] summaries = results.getMatchResults();
+		for (MatchDocumentSummary summary : summaries) {
+			MatchLocation[] locations = summary.getMatchLocations();
+			for (MatchLocation location : locations) {
+				assertNotNull(location.getAllSnippetText());
+			}
+		}
+
+		assertNotNull(summaries);
+	}
+
+	@Test
+	public void testJsonSearch() throws IOException {
+		QueryManager queryMgr = Common.client.newQueryManager();
+		KeyValueQueryDefinition qdef = queryMgr.newKeyValueDefinition(null);
+
+		qdef.put(queryMgr.newKeyLocator("firstKey"), "first value");
+		SearchHandle results = queryMgr.search(qdef, new SearchHandle());
+		assertNotNull(results);
+		assertFalse(results.getMetrics().getTotalTime() == -1);
+
+		MatchDocumentSummary[] summaries = results.getMatchResults();
+		for (MatchDocumentSummary summary : summaries) {
+			MatchLocation[] locations = summary.getMatchLocations();
+			for (MatchLocation location : locations) {
+				assertNotNull(location.getAllSnippetText());
+			}
+		}
+
+		assertNotNull(summaries);
+	}
 }
