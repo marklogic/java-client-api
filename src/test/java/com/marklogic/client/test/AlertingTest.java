df33f6818de38e87d34e735fff2f7f175e388739
cgreer <cgreer@62cac252-8da6-4816-9e9d-6dc37b19578c>
2013-06-04 15:35:27 +0000

    unit test update
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/trunk@143296 62cac252-8da6-4816-9e9d-6dc37b19578c

--- src/test/java/com/marklogic/client/test/AlertingTest.java
+++ src/test/java/com/marklogic/client/test/AlertingTest.java
@@ -286,7 +286,7 @@ public class AlertingTest {
 		BytesHandle bHandle = ruleManager.readRule("javatestrule",
 				new BytesHandle().withFormat(Format.JSON));
 		assertEquals(
-				"{\"rule\":{\"name\":\"javatestrule\", \"description\":\"rule to demonstrate REST alerting\", \"search\":{\"qtext\":\"favorited:true\", \"options\":{\"constraint\":[{\"name\":\"favorited\", \"value\":{\"element\":{\"ns\":\"\", \"name\":\"favorited\"}}}]}}, \"rule-metadata\":null}}",
+				"{\"rule\":{\"name\":\"javatestrule\", \"description\":\"rule to demonstrate REST alerting\", \"search\":{\"qtext\":[\"favorited:true\"], \"options\":{\"constraint\":[{\"name\":\"favorited\", \"value\":{\"element\":{\"ns\":\"\", \"name\":\"favorited\"}}}]}}, \"rule-metadata\":null}}",
 				new String(bHandle.get()));
 
 		ruleManager.delete("javatestrule");
