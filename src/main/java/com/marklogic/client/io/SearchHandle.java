60beb8cbc8b71edfca542fe8d443f770ff35f116
ehennum <ehennum@62cac252-8da6-4816-9e9d-6dc37b19578c>
2014-03-18 17:41:35 +0000

    cleanup unused import
    git-svn-id: svn+ssh://svn.marklogic.com/project/engsvn/client-api/java/branches/b2_0@164239 62cac252-8da6-4816-9e9d-6dc37b19578c

--- src/main/java/com/marklogic/client/io/SearchHandle.java
+++ src/main/java/com/marklogic/client/io/SearchHandle.java
@@ -19,7 +19,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
