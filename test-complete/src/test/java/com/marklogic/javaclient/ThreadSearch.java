eb3b5f2c8ef711f60aedd3a88805664d05c566f3
gvaidees <gvaidees@marklogic.com>
2014-08-21 09:07:36 -0700

    Code cleanup

--- test-complete/src/test/java/com/marklogic/javaclient/ThreadSearch.java
+++ test-complete/src/test/java/com/marklogic/javaclient/ThreadSearch.java
@@ -11,52 +11,53 @@ import com.marklogic.client.io.SearchHandle;
 
 public class ThreadSearch extends Thread {
 
-    String msg;
-    long totalResultsArray[] = new long[10];
-    long totalAllResults = 0;
+	String msg;
+	long totalResultsArray[] = new long[10];
+	long totalAllResults = 0;
 
-    public void run()
-    {	
-    	long totalResults = 0;
-    	
-        DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8011, "rest-reader", "x", Authentication.DIGEST);
-        
-        QueryManager queryMgr = client.newQueryManager();
-        StringQueryDefinition querydef = queryMgr.newStringDefinition(null);
-        
+	public void run()
+	{	
+		long totalResults = 0;
+
+		DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8011, "rest-reader", "x", Authentication.DIGEST);
+
+		QueryManager queryMgr = client.newQueryManager();
+		StringQueryDefinition querydef = queryMgr.newStringDefinition(null);
+
 		// create handle
 		SearchHandle resultsHandle = new SearchHandle();        
 
-        for(int i=1; i<=10; i++)
-        {
-            System.out.println("Searching document " + i + " from: " + msg);
-            	
-            // search docs
-            querydef.setCriteria("alert");
-            queryMgr.search(querydef, resultsHandle);
-            
-            totalResults = resultsHandle.getTotalResults();
-            
-            System.out.println("Results: " + totalResults + " documents returned");
-            
-            totalResultsArray[i-1] = totalResults;
-            
-            totalAllResults = totalAllResults + totalResults;
-            
-            Random rand = new Random();
-            int r = rand.nextInt(3000) + 1000;
-            
-            try {
-				Thread.sleep(r);
-			} catch (InterruptedException e) { e.printStackTrace(); }
-        }
-        
-        // release client
-     	client.release();
-    }
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("Searching document " + i + " from: " + msg);
 
-    ThreadSearch(String mg)
-    {
-        msg=mg;
-    }
+			// search docs
+			querydef.setCriteria("alert");
+			queryMgr.search(querydef, resultsHandle);
+
+			totalResults = resultsHandle.getTotalResults();
+
+			System.out.println("Results: " + totalResults + " documents returned");
+
+			totalResultsArray[i-1] = totalResults;
+
+			totalAllResults = totalAllResults + totalResults;
+
+			Random rand = new Random();
+			int r = rand.nextInt(3000) + 1000;
+
+			try {
+				Thread.sleep(r);
+			} 
+			catch (InterruptedException e) { e.printStackTrace(); }
+		}
+
+		// release client
+		client.release();
+	}
+
+	ThreadSearch(String mg)
+	{
+		msg=mg;
+	}
 }
--- test-complete/src/test/java/com/marklogic/javaclient/ThreadWrite.java
+++ test-complete/src/test/java/com/marklogic/javaclient/ThreadWrite.java
@@ -11,40 +11,41 @@ import com.marklogic.client.io.FileHandle;
 
 public class ThreadWrite extends Thread{
 
-    String msg;
+	String msg;
 
-    public void run()
-    {	
-    	String filename = "flipper.xml";
-    	
-        DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8011, "rest-writer", "x", Authentication.DIGEST);
-        
-        File file = new File("src/test/java/com/marklogic/javaclient/data/" + filename);
-        
-        XMLDocumentManager docMgr = client.newXMLDocumentManager();
+	public void run()
+	{	
+		String filename = "flipper.xml";
 
-        for(int i=1; i<=15; i++)
-        {
-            System.out.println("Writing document " + i + " from: " + msg);
-            	
-            // write docs
-            String docId = "/multithread-write/filename" + i + ".xml";
-            docMgr.write(docId, new FileHandle().with(file));
-            
-            Random rand = new Random();
-            int r = rand.nextInt(2000) + 1000;
-            
-            try {
+		DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8011, "rest-writer", "x", Authentication.DIGEST);
+
+		File file = new File("src/test/java/com/marklogic/javaclient/data/" + filename);
+
+		XMLDocumentManager docMgr = client.newXMLDocumentManager();
+
+		for(int i=1; i<=15; i++)
+		{
+			System.out.println("Writing document " + i + " from: " + msg);
+
+			// write docs
+			String docId = "/multithread-write/filename" + i + ".xml";
+			docMgr.write(docId, new FileHandle().with(file));
+
+			Random rand = new Random();
+			int r = rand.nextInt(2000) + 1000;
+
+			try {
 				Thread.sleep(r);
-			} catch (InterruptedException e) { e.printStackTrace(); }
-        }
-        
-        // release client
-     	client.release();
-    }
+			} 
+			catch (InterruptedException e) { e.printStackTrace(); }
+		}
 
-    ThreadWrite(String mg)
-    {
-        msg=mg;
-    }
+		// release client
+		client.release();
+	}
+
+	ThreadWrite(String mg)
+	{
+		msg=mg;
+	}
 }
