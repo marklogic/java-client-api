f8c4be34369804b3c6e135b1afa9a60fbcb3f882
Ajit George <Ajit.George@MarkLogic.com>
2014-11-05 00:31:58 -0800

    New classes and test methods to verify automatic range path index annotation.

---
+++ test-complete/src/test/java/com/marklogic/javaclient/ArtifactIndexedOnDateTime.java
@@ -0,0 +1,67 @@
+package com.marklogic.javaclient;
+
+import java.util.Calendar;
+
+import com.marklogic.client.pojo.annotation.Id;
+import com.marklogic.client.pojo.annotation.PathIndexProperty;
+import com.marklogic.client.pojo.annotation.PathIndexProperty.ScalarType;
+
+/*
+ * This class is similar to the Artifact class. It is used to test path range index using the DateTime field
+ * Property name been annotated with @Id.
+ */
+public class ArtifactIndexedOnDateTime {
+	@Id
+	public String name;
+	public long id;
+	private Company manufacturer;
+	private int inventory;
+
+	@PathIndexProperty(scalarType = ScalarType.DATETIME)
+	private Calendar expiryDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public ArtifactIndexedOnDateTime setId(long id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArtifactIndexedOnDateTime setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public Company getManufacturer() {
+		return manufacturer;
+	}
+
+	public ArtifactIndexedOnDateTime setManufacturer(Company manufacturer) {
+		this.manufacturer = manufacturer;
+		return this;
+	}
+
+	public int getInventory() {
+		return inventory;
+	}
+
+	public ArtifactIndexedOnDateTime setInventory(int inventory) {
+		this.inventory = inventory;
+		return this;
+	}
+
+	public Calendar getExpiryDate() {
+		return expiryDate;
+	}
+
+	public ArtifactIndexedOnDateTime setExpiryDate(Calendar expiryDate) {
+		this.expiryDate = expiryDate;
+		return this;
+	}
+}
