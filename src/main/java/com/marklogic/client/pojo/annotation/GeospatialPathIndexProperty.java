c208dbba7af8fe352c6d1be6674fe34b19ea6453
sammefford <sam@mefford.org>
2014-11-12 15:59:25 -0700

    add javadocs

--- src/main/java/com/marklogic/client/pojo/annotation/GeospatialPathIndexProperty.java
+++ src/main/java/com/marklogic/client/pojo/annotation/GeospatialPathIndexProperty.java
@@ -18,6 +18,46 @@ package com.marklogic.client.pojo.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/** Use this annotation to specify that a Geospatial Path Index should
+ * be created for this pojo property. The value should follow the rules
+ * for any Geospatial Path Index in MarkLogic Server with coordinate
+ * system "wgs84" and point format "point". Specifically, the value 
+ * should be in "{latititude} {longitude}" point format where {latitude}
+ * and {longitude} are numeric geospatial wgs84 position values.
+ *
+ * This annotation can be associated with a public field:
+ * <pre>    import com.marklogic.client.pojo.annotation.GeospatialPathIndexProperty;
+ *    public class MyClass {
+ *      <b>{@literal @}GeospatialPathIndexProperty</b>
+ *      public String myGeoProperty;
+ *    }</pre>
+ *
+ * or with a public getter method:
+ * <pre>
+ *    public class MyClass {
+ *      private String myGeoProperty;
+ *      <b>{@literal @}GeospatialPathIndexProperty</b>
+ *      public String getMyGeoProperty() {
+ *        return myGeoProperty;
+ *      }
+ *      // ... setter methods ...
+ *    }</pre>
+*
+ * or with a public setter method:
+ * <pre>
+ *    public class MyClass {
+ *      private String myGeoProperty;
+ *      // ... getter methods ...
+ *      <b>{@literal @}GeospatialPathIndexProperty</b>
+ *      public void setMyGeoProperty(String myGeoProperty) {
+ *        this.myGeoProperty = myGeoProperty;
+ *      }
+ *    }</pre>
+ * Run
+ * {@link com.marklogic.client.pojo.util.GenerateIndexConfig} to generate
+ * a package that can be used by administrators to create the indexes in
+ * MarkLogic Server.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GeospatialPathIndexProperty {
 }
--- src/main/java/com/marklogic/client/pojo/annotation/PathIndexProperty.java
+++ src/main/java/com/marklogic/client/pojo/annotation/PathIndexProperty.java
@@ -18,6 +18,49 @@ package com.marklogic.client.pojo.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/** Use this annotation to specify that a Path Index (required for range
+ * queries) should be created for this pojo property.  A path index is
+ * used rather than an element range index because we can restrict matches
+ * to only properties that are direct children of this class type.  We
+ * do this by including the class name in the path specification on the
+ * path index.
+ *
+ * This annotation can be associated with a public field:
+ * <pre>    import com.marklogic.client.pojo.annotation.PathIndexProperty;
+ *    public class MyClass {
+ *      <b>{@literal @}PathIndexProperty</b>
+ *      public Long myLongValue;
+ *    }</pre>
+ *
+ * or with a public getter method:
+ * <pre>
+ *    public class MyClass {
+ *      private Long myLongValue;
+ *      <b>{@literal @}PathIndexProperty</b>
+ *      public Long getMyLongValue() {
+ *        return myLongValue;
+ *      }
+ *      // ... setter methods ...
+ *    }</pre>
+*
+ * or with a public setter method:
+ * <pre>
+ *    public class MyClass {
+ *      private Long myLongValue;
+ *
+ *      // ... getter methods ...
+ *
+ *      <b>{@literal @}PathIndexProperty</b>
+ *      public void setMyLongValue(Long myLongValue) {
+ *        this.myLongValue = myLongValue;
+ *      }
+ *    }</pre>
+ * Run
+ * {@link com.marklogic.client.pojo.util.GenerateIndexConfig} to generate
+ * a package that can be used by administrators to create the indexes in
+ * MarkLogic Server.
+ * @see com.marklogic.client.pojo.PojoQueryBuilder#range
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PathIndexProperty {
     ScalarType scalarType();
