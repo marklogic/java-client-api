1fc71ea925bed081554690c78025cf1b5ccf2e27
Sam Mefford <sam.mefford@marklogic.com>
2014-12-12 16:30:32 -0700

    enhance #223 - Kim recommends the warning come before the example

--- src/main/java/com/marklogic/client/pojo/util/GenerateIndexConfig.java
+++ src/main/java/com/marklogic/client/pojo/util/GenerateIndexConfig.java
@@ -37,10 +37,18 @@ import com.marklogic.client.pojo.annotation.GeospatialPathIndexProperty;
 import com.marklogic.client.pojo.annotation.Id;
 import com.marklogic.client.pojo.annotation.PathIndexProperty;
 
-/** Generates a MarkLogic index configuration file in JSON format describing the indexes 
+/** <p>Generates a MarkLogic index configuration file in JSON format describing the indexes 
  * required by the annotations on the specific classes.  
  * The output can be used by an administrator to create indexes in the database
- * using the Management REST API, for example:
+ * using the Management REST API.</p>
+ *
+ * <p><b>WARNING!</b> Applying this generated index file via management API will overwrite
+ * existing indexes!  Only use this as-is if there are no other indexes on your
+ * database that you want to keep.  To add these indexes to other indexes please get
+ * the configuration for existing indexes from the management API and add these to that
+ * file before applying.</p>
+ *
+ * <p>Usage example:</p>
  * <pre>
  *     java com.marklogic.client.pojo.util.GenerateIndexConfig 
  *       -classes "com.marklogic.client.test.City com.marklogic.client.test.Country" 
@@ -51,11 +59,6 @@ import com.marklogic.client.pojo.annotation.PathIndexProperty;
  *       -d '@cityIndexes.json' \
  *       -X PUT 'http://localhost:8002/manage/LATEST/databases/java-unittest/properties'
  * </pre>
- * <b>WARNING!</b> Applying this generated index file via maangement API will overwrite
- * existing indexes!  Only use this as-is if there are no other indexes on your
- * database that you want to keep.  To add these indexes to other indexes please get
- * the configuration for existing indexes from the management API and add these to that
- * file before applying.
  */
 public class GenerateIndexConfig {
     /**
