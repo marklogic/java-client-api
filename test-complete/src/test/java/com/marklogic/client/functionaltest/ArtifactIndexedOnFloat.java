4ed9e5421cc050ce435a1050830831fb23cd8bfd
Ajit George <Ajit.George@MarkLogic.com>
2014-12-30 15:33:24 -0800

    Additional Test cases for Git issue 222

---
+++ test-complete/src/test/java/com/marklogic/client/functionaltest/ArtifactIndexedOnFloat.java
@@ -0,0 +1,76 @@
+package com.marklogic.client.functionaltest;
+
+import java.util.Calendar;
+
+import com.marklogic.client.pojo.annotation.Id;
+import com.marklogic.client.pojo.annotation.PathIndexProperty;
+import com.marklogic.client.pojo.annotation.PathIndexProperty.ScalarType;
+
+/*
+ * This class is similar to the Artifact class. It is used to test path range index using the Float type.
+ * Property name been annotated with @Id.
+ */
+public class ArtifactIndexedOnFloat {
+	@Id
+	public String name;
+	public long id;
+	private Company manufacturer;
+	private int inventory;
+	@PathIndexProperty(scalarType = ScalarType.FLOAT)
+	private Float price;
+	
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	private Calendar expiryDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public ArtifactIndexedOnFloat setId(long id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArtifactIndexedOnFloat setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public Company getManufacturer() {
+		return manufacturer;
+	}
+
+	public ArtifactIndexedOnFloat setManufacturer(Company manufacturer) {
+		this.manufacturer = manufacturer;
+		return this;
+	}
+
+	public int getInventory() {
+		return inventory;
+	}
+
+	public ArtifactIndexedOnFloat setInventory(int inventory) {
+		this.inventory = inventory;
+		return this;
+	}
+
+	public Calendar getExpiryDate() {
+		return expiryDate;
+	}
+
+	public ArtifactIndexedOnFloat setExpiryDate(Calendar expiryDate) {
+		this.expiryDate = expiryDate;
+		return this;
+	}
+}
---
+++ test-complete/src/test/java/com/marklogic/client/functionaltest/ArtifactIndexedOnInteger.java
@@ -0,0 +1,56 @@
+package com.marklogic.client.functionaltest;
+
+import com.marklogic.client.pojo.annotation.Id;
+import com.marklogic.client.pojo.annotation.PathIndexProperty;
+import com.marklogic.client.pojo.annotation.PathIndexProperty.ScalarType;
+
+/*
+ * This class is similar to the Artifact class. It is used to test path range index 
+ * using the inventory field with Integer type.
+ * Property name been annotated with @Id.
+ */
+public class ArtifactIndexedOnInteger {
+	@Id
+	public String name;
+	public long id;
+	private Company manufacturer;
+
+	@PathIndexProperty(scalarType = ScalarType.INT)
+	private Integer inventory;
+
+	public long getId() {
+		return id;
+	}
+
+	public ArtifactIndexedOnInteger setId(long id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArtifactIndexedOnInteger setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public Company getManufacturer() {
+		return manufacturer;
+	}
+
+	public ArtifactIndexedOnInteger setManufacturer(Company manufacturer) {
+		this.manufacturer = manufacturer;
+		return this;
+	}
+
+	public Integer getInventory() {
+		return inventory;
+	}
+
+	public ArtifactIndexedOnInteger setInventory(Integer inventory) {
+		this.inventory = inventory;
+		return this;
+	}
+}
