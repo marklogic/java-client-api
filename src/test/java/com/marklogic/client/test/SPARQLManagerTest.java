3d0be36b118b2c3b117e7e44e95321310ac00bb9
Sam Mefford <sam.mefford@marklogic.com>
2015-07-14 15:12:59 -0600

    split one test method into many; add delete to cleanup document written
    (cherry picked from commit 796986022c3457c295f4ffb7b70c5a644cf2c4f8)

--- src/test/java/com/marklogic/client/test/SPARQLManagerTest.java
+++ src/test/java/com/marklogic/client/test/SPARQLManagerTest.java
@@ -46,20 +46,20 @@ public class SPARQLManagerTest {
     private static String ontology = 
             "<http://example.org/p1> <http://www.w3.org/2000/01/rdf-schema#range> <http://example.org/C1> . \n" +
             "<http://example.org/p2> <http://www.w3.org/2000/01/rdf-schema#domain> <http://example.org/C1> .";
-
-    ;
-    
+    private static String expectedFirstResult =
+            "{s:{value:'http://example.org/s1', type:'uri'}," +
+            "p:{value:'http://example.org/p1', type:'uri'}," +
+            "o:{value:'http://example.org/o1', type:'uri'}}";
     private static ObjectMapper mapper = new ObjectMapper()
         .configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
         .configure(Feature.ALLOW_SINGLE_QUOTES, true);
     private static SPARQLQueryManager smgr;
     private static GraphManager gmgr;
-    
 
     @BeforeClass
     public static void beforeClass() {
         Common.connect();
-//        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "debug");
+        //System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "debug");
         gmgr = Common.client.newGraphManager();
         String nTriples = triple1 + "\n" + triple2;
         gmgr.write(graphUri, new StringHandle(nTriples).withMimetype("application/n-triples"));
@@ -68,25 +68,27 @@ public class SPARQLManagerTest {
 
     @AfterClass
     public static void afterClass() {
+        gmgr.delete(graphUri);
         Common.release();
     }
 
     @Test
-    public void testSPARQL() throws Exception {
+    public void testSPARQLWithLimit() throws Exception {
         SPARQLQueryDefinition qdef1 = smgr.newQueryDefinition("select ?s ?p ?o { ?s ?p ?o } limit 1");
+        qdef1.setCollections(graphUri);
         JsonNode jsonResults = smgr.executeSelect(qdef1, new JacksonHandle()).get();
-        String expectedFirstResult =
-            "{s:{value:'http://example.org/s1', type:'uri'}," +
-            "p:{value:'http://example.org/p1', type:'uri'}," +
-            "o:{value:'http://example.org/o1', type:'uri'}}";
         int numResults = jsonResults.path("results").path("bindings").size();
         // because we said 'limit 1' we should only get one result
         assertEquals(1, numResults);
         JsonNode firstResult = jsonResults.path("results").path("bindings").path(0);
         assertEquals(mapper.readTree(expectedFirstResult), firstResult);
+    }
 
+    @Test
+    public void testSPARQLWithTwoResults() throws Exception {
         SPARQLQueryDefinition qdef2 = smgr.newQueryDefinition("select ?s ?p ?o { ?s ?p ?o } limit 100");
-        jsonResults = smgr.executeSelect(qdef2, new JacksonHandle()).get();
+        qdef2.setCollections(graphUri);
+        JsonNode jsonResults = smgr.executeSelect(qdef2, new JacksonHandle()).get();
         JsonNode tuples = jsonResults.path("results").path("bindings");
         // loop through the "bindings" array (we would call each row a tuple)
         for ( int i=0; i < tuples.size(); i++ ) {
@@ -110,7 +112,10 @@ public class SPARQLManagerTest {
                 }
             }
         }
+    }
 
+    @Test
+    public void testSPARQLWithBindings() throws Exception {
         SPARQLQueryDefinition qdef3 = smgr.newQueryDefinition("construct { ?s ?p ?o } where  { <subjectExample0> ?p ?o } ");
         StringHandle results1 = smgr.executeConstruct(qdef3, new StringHandle());
 
@@ -129,6 +134,7 @@ public class SPARQLManagerTest {
         assertEquals(1, numResults2);
         JsonNode firstResult2 = jsonResults2.path("results").path("bindings").path(0);
         assertEquals(mapper.readTree(expectedFirstResult), firstResult2);
+    }
 
         /*
         // to configure inference
@@ -137,7 +143,6 @@ public class SPARQLManagerTest {
         qdef4 = qdef4.withRuleset(SPARQLRuleset.ruleset("custom.rules"));
         // use a start and page length, and no transaction
         JsonNode results2 = smgr.executeSelect(qdef4, 1, 100, null);
-        */
 
         // To invoke an update
         SPARQLQueryDefinition qdef5 = smgr.newQueryDefinition("insert data { ... }");
@@ -149,8 +154,7 @@ public class SPARQLManagerTest {
         QueryDefinition structuredQuery = new StructuredQueryBuilder().term("test");
 
         SPARQLQueryDefinition qdef7 = smgr.newQueryDefinition("insert data { ... }").withUpdatePermission("rest-reader", Capability.UPDATE).withStructuredQuery(structuredQuery);
-
-    }
+        */
 
     @Test
     public void testPagination() throws Exception {
@@ -178,12 +182,9 @@ public class SPARQLManagerTest {
         String uri2 = bindings.get(0).get("s").get("value").asText();
         assertNotEquals(uri1, uri2);
     }
-    
+
     @Test
     public void testInference() throws Exception {
-        //private static String triple1 = "<http://example.org/s1> <http://example.org/p1> <http://example.org/o1>.";
-        //private static String triple2 = "<http://example.org/s2> <http://example.org/p2> <http://example.org/o2>.";
-        // private static String ontology = "<http://example.org/C1> <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://example.org/C2> . <http://example.org/s1> <http://www.w3.org/2000/01/rdf-schema#range> <http://example.org/C1> .";
         gmgr.write("/ontology", new StringHandle(ontology).withMimetype("application/n-triples"));
         SPARQLQueryDefinition qdef = smgr.newQueryDefinition(
                 "SELECT ?s { ?s a <http://example.org/C1>  }");
@@ -192,11 +193,11 @@ public class SPARQLManagerTest {
         qdef.setRulesets(SPARQLRuleset.RANGE);
         results = smgr.executeSelect(qdef, new JacksonHandle()).get();
         assertEquals(1, results.path("results").path("bindings").size());
-        
+
         qdef.setRulesets(SPARQLRuleset.RANGE, SPARQLRuleset.DOMAIN);
         results = smgr.executeSelect(qdef, new JacksonHandle()).get();
         assertEquals(2, results.path("results").path("bindings").size());
-        
+
         gmgr.delete("/ontology");
     }
 }
