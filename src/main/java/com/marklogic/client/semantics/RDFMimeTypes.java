db041ead68c76c859d5c1c37ad31968cf651f25d
Sam Mefford <sam.mefford@marklogic.com>
2015-07-20 13:02:45 -0600

    fix a type in RDFJSON type
    (cherry picked from commit 0a22b1e94fb3e600243e1119f9866c49b875c722)

--- src/main/java/com/marklogic/client/semantics/RDFMimeTypes.java
+++ src/main/java/com/marklogic/client/semantics/RDFMimeTypes.java
@@ -11,7 +11,7 @@ public final class RDFMimeTypes {
     public final static String TURTLE    = "text/turtle";
     public final static String N3        = "text/n3";
     public final static String RDFXML    = "application/rdf+xml";
-    public final static String RDFJSON   = "applicatoin/rdf+json";
+    public final static String RDFJSON   = "application/rdf+json";
     public final static String NQUADS    = "application/n-quads";
     public final static String TRIG      = "text/trig";
     public final static String TRIPLEXML = "application/vnd.marklogic.triples+xml";
