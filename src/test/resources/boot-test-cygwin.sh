c47f6928159cbc1058eebb25c5c4efabcad75e77
Sam Mefford <sam.mefford@marklogic.com>
2015-08-05 09:48:29 -0600

    fix error preventing users from being created; add windows-friendly bootstrap and teardown scripts

---
+++ src/test/resources/boot-test-cygwin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DEFAULT_M2=$USERPROFILE/.m2/repository
+M2_REPO=${1:-$DEFAULT_M2}
+
+java -cp "target/test-classes;target/classes;$M2_REPO/org/apache/httpcomponents/httpclient/4.1.1/httpclient-4.1.1.jar;$M2_REPO/org/apache/httpcomponents/httpcore/4.1/httpcore-4.1.jar;$M2_REPO/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar;$M2_REPO/org/slf4j/slf4j-api/1.7.4/slf4j-api-1.7.4.jar" com.marklogic.client.test.util.TestServerBootstrapper
