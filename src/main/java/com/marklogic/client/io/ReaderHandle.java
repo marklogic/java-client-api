da8094196018da3fcfca0acdec8e50f67c72ed51
Sam Mefford <sam.mefford@marklogic.com>
2015-08-07 16:13:04 -0600

    fix #295 and #91 - add close methods where they add convenience or are needed, add javadocs where devs should close things
    (cherry picked from commit 0eef19337963a6f3cf38c0f05ba534ae12a92e13)

--- src/main/java/com/marklogic/client/io/ReaderHandle.java
+++ src/main/java/com/marklogic/client/io/ReaderHandle.java
@@ -17,6 +17,7 @@ package com.marklogic.client.io;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,6 +29,9 @@ import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CoderResult;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.marklogic.client.MarkLogicIOException;
 import com.marklogic.client.io.marker.BufferableHandle;
@@ -46,10 +50,11 @@ import com.marklogic.client.io.marker.XMLReadHandle;
 import com.marklogic.client.io.marker.XMLWriteHandle;
 
 /**
- * A Reader Handle represents a character content as a reader
- * for reading to or writing from the database.
- * 
- * When finished with the reader, close the reader to release the resources.
+ * <p>A Reader Handle represents a character content as a reader
+ * for reading to or writing from the database.</p>
+ *
+ * <p>Either call {@link #close} or {@link #get}.close() when finished with this handle
+ * to release the resources.</p>
  */
 public class ReaderHandle
 	extends BaseHandle<InputStream, OutputStreamSender>
@@ -59,8 +64,11 @@ public class ReaderHandle
 		XMLReadHandle, XMLWriteHandle,
 		StructureReadHandle, StructureWriteHandle,
 		QuadsWriteHandle,
-		TriplesReadHandle, TriplesWriteHandle
+		TriplesReadHandle, TriplesWriteHandle,
+		Closeable
 {
+	static final private Logger logger = LoggerFactory.getLogger(InputStreamHandle.class);
+
 	final static private int BUFFER_SIZE = 8192;
 
     private Reader content;
@@ -237,5 +245,18 @@ public class ReaderHandle
 		}
 
 		out.flush();
+	}
+
+	/** Either call close() or get().close() when finished with this handle to close the underlying Reader.
+	 */
+	public void close() {
+		if ( content != null ) {
+			try {
+				content.close();
+			} catch (IOException e) {
+				logger.error("Failed to close underlying InputStream",e);
+				throw new MarkLogicIOException(e);
+			}
+		}
 	}
 }
---
+++ src/test/java/com/marklogic/client/test/ClosingHandlesTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012-2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.client.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.Closeable;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBContext;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.marklogic.client.extra.dom4j.DOM4JHandle;
+import com.marklogic.client.extra.gson.GSONHandle;
+import com.marklogic.client.extra.jdom.JDOMHandle;
+import com.marklogic.client.extra.xom.XOMHandle;
+import com.marklogic.client.impl.HandleAccessor;
+import com.marklogic.client.io.DOMHandle;
+import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.InputSourceHandle;
+import com.marklogic.client.io.InputStreamHandle;
+import com.marklogic.client.io.JAXBHandle;
+import com.marklogic.client.io.JacksonDatabindHandle;
+import com.marklogic.client.io.JacksonHandle;
+import com.marklogic.client.io.JacksonParserHandle;
+import com.marklogic.client.io.QueryOptionsHandle;
+import com.marklogic.client.io.QueryOptionsListHandle;
+import com.marklogic.client.io.ReaderHandle;
+import com.marklogic.client.io.SearchHandle;
+import com.marklogic.client.io.SourceHandle;
+import com.marklogic.client.io.TuplesHandle;
+import com.marklogic.client.io.ValuesHandle;
+import com.marklogic.client.io.ValuesListHandle;
+import com.marklogic.client.io.XMLEventReaderHandle;
+import com.marklogic.client.io.XMLStreamReaderHandle;
+import com.marklogic.client.io.marker.AbstractReadHandle;
+
+public class ClosingHandlesTest {
+    private static class MockInputStream extends ByteArrayInputStream {
+        public MockInputStream(byte[] buf) {
+            super(buf);
+        }
+
+        private boolean isClosed = false;
+        public void close() throws IOException {
+            super.close();
+            isClosed = true;
+        }
+
+        public boolean isClosed() {
+            return isClosed;
+        }
+    }
+
+    @Test
+    public void closeHandles() throws Exception {
+        JAXBContext context = JAXBContext.newInstance(City.class);
+        validateClosingHandleClosesUnderlyingStream(
+            new DocumentMetadataHandle(), "<metadata xmlns='http://marklogic.com/rest-api'/>");
+        validateClosingHandleClosesUnderlyingStream(new DOMHandle(), "<xml/>");
+        validateClosingHandleClosesUnderlyingStream(new DOM4JHandle(), "<xml/>");
+        validateClosingHandleClosesUnderlyingStream(new GSONHandle(), "null");
+        validateClosingHandleClosesUnderlyingStream(new InputSourceHandle(), "testing");
+        validateClosingHandleClosesUnderlyingStream(new InputStreamHandle(), "testing");
+        validateClosingHandleClosesUnderlyingStream(new JacksonHandle(), "null");
+        validateClosingHandleClosesUnderlyingStream(
+            new JacksonDatabindHandle<Object>(Object.class), "null");
+        validateClosingHandleClosesUnderlyingStream(new JacksonParserHandle(), "null");
+        validateClosingHandleClosesUnderlyingStream(
+            new JAXBHandle<Object>(context), "<city><population>0</population></city>");
+        validateClosingHandleClosesUnderlyingStream(new JDOMHandle(), "<xml/>");
+        validateClosingHandleClosesUnderlyingStream(
+            new QueryOptionsHandle(), "<options xmlns='http://marklogic.com/appservices/search'/>");
+        validateClosingHandleClosesUnderlyingStream(
+            new QueryOptionsListHandle(), "<query-options xmlns='http://marklogic.com/rest-api'/>");
+        validateClosingHandleClosesUnderlyingStream(new ReaderHandle(), "testing");
+        validateClosingHandleClosesUnderlyingStream(
+            new SearchHandle(), "<response page-length='0' start='0' xmlns='http://marklogic.com/appservices/search'/>");
+        validateClosingHandleClosesUnderlyingStream(new SourceHandle(), "testing");
+        validateClosingHandleClosesUnderlyingStream(
+            new TuplesHandle(), "<values-response xmlns='http://marklogic.com/appservices/search'/>");
+        validateClosingHandleClosesUnderlyingStream(
+            new ValuesHandle(), "<values-response xmlns='http://marklogic.com/appservices/search'/>");
+        validateClosingHandleClosesUnderlyingStream(
+            new ValuesListHandle(), "<values-list xmlns='http://marklogic.com/rest-api'/>");
+        validateClosingHandleClosesUnderlyingStream(new XMLEventReaderHandle(), "<xml/>");
+        validateClosingHandleClosesUnderlyingStream(new XMLStreamReaderHandle(), "<xml/>");
+        validateClosingHandleClosesUnderlyingStream(new XOMHandle(), "<xml/>");
+    }
+
+    private void validateClosingHandleClosesUnderlyingStream(
+            AbstractReadHandle handle, String content)
+        throws Exception
+    {
+        MockInputStream stream = new MockInputStream(content.getBytes());
+        HandleAccessor.receiveContent(handle, stream);
+        if ( handle instanceof Closeable ) {
+            ((Closeable) handle).close();
+        }
+        assertTrue("Handle " + handle.getClass().getName() + " did not close underlying InputStream", stream.isClosed());
+
+    }
+
+}
