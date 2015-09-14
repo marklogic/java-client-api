3d96cbe57d6f3b6448686c9ad3d647deebb04d34
Sam Mefford <sam.mefford@marklogic.com>
2015-09-14 08:44:32 -0600

    more javadocs
    (cherry picked from commit 54ec39f07fdab3e17b647c7f1d9abba34ff28002)

--- src/main/java/com/marklogic/client/semantics/SPARQLQueryManager.java
+++ src/main/java/com/marklogic/client/semantics/SPARQLQueryManager.java
@@ -29,11 +29,12 @@ import com.marklogic.client.semantics.Capability;
  * <p>A manager for executing SPARQL queries in MarkLogic Server and retrieving
  * the results.
  *
- * <p>For example:</p>
+ * <p>For example perform a SPARQL SELECT:</p>
+ *
  * <pre>    SPARQLQueryManager sparqlMgr = databaseClient.newSPARQLQueryManager();
  *    String sparql = "SELECT * WHERE { ?s ?p ?o } LIMIT 10";
  *    SPARQLQueryDefinition query = sparqlMgr.newQueryDefinition(sparql)
- *        .withBinding("o", "http://example.org/o1");
+ *        .withBinding("o", "http://example.org/object1");
  *    JacksonHandle handle = new JacksonHandle();
  *    handle.setMimetype(SPARQLMimeTypes.SPARQL_JSON);
  *    JacksonHandle results = sparqlMgr.executeSelect(query, handle);
@@ -44,29 +45,47 @@ import com.marklogic.client.semantics.Capability;
  *        ...
  *    }</pre>
  *
+ * <p>Or perform a SPARQL CONSTRUCT:</p>
+ *
+ * <pre>    String sparql = "CONSTRUCT { &lt;a&gt; &lt;b&gt; &lt;c&gt; } WHERE { ?s ?p ?o } LIMIT 10";
+ *    SPARQLQueryDefinition query = sparqlMgr.newQueryDefinition(sparql)
+ *        .withBinding("o", "http://example.org/object1");
+ *    JacksonHandle handle = new JacksonHandle();
+ *    handle.setMimetype(RDFMimeTypes.RDFJSON);
+ *    JacksonHandle triples = sparqlMgr.executeConstruct(query, handle);
+ *    JsonNode a = triples.get().path("a");
+ *    JsonNode b = a.path("b");
+ *    JsonNode c = b.get(0).path("value");</pre>
+ *
  * <p>Each new instance of SPARQLQueryManager is created by
- * {@link DatabaseClient#newSPARQLQueryManager}.  While this example uses
+ * {@link DatabaseClient#newSPARQLQueryManager}.  While these examples use
  * JacksonHandle, any SPARQLResultsReadHandle may be used--including custom
- * handles.  While {@link JSONReadHandle}s will need to use SPARQL_JSON mimetype,
- * and {@link XMLReadHandle}s will need to use SPARQL_XML mimetype,
- * most {@link TriplesReadHandle}s accept any text and can therefore accept
- * results in any of the RDFMimeTypes or SPARQLMimeTypes.
+ * handles.  For executeSelect {@link JSONReadHandle}s will need to use {@link
+ * SPARQLMimeTypes#SPARQL_JSON} mimetype, and {@link XMLReadHandle}s will need
+ * to use {@link SPARQLMimeTypes#SPARQL_XML} mimetype, other {@link
+ * SPARQLResultsReadHandle}s accept any text and can therefore accept results
+ * in any of the SPARQLMimeTypes.  For executeDescribe and executeConstruct
+ * {@link JSONReadHandle}s will need to use {@link RDFMimeTypes#RDFJSON}
+ * mimetype, and {@link XMLReadHandle}s will need to use {@link
+ * RDFMimeTypes#RDFXML} mimetype, other {@link TriplesReadHandle}s accept any
+ * text and can therefore accept results in any of the RDFMimeTypes.
  *
- * <p>SPARQLQueryManager is
- * thread-safe other than setPageLength. In other words the only state
- * maintained by an instance is the page length.  Common usage is to call
- * setPageLength only once then use the instance across many threads.  If you
- * intend to call setPageLength from multiple threads, create a new
- * SPARQLQueryManager for each thread.</p>
+ * <p>SPARQLQueryManager is thread-safe other than setPageLength. In other
+ * words the only state maintained by an instance is the page length.  Common
+ * usage is to call setPageLength only once then use the instance across many
+ * threads.  If you intend to call setPageLength from multiple threads, create
+ * a new SPARQLQueryManager for each thread.</p>
  *
- * <p>For details about RDF, SPARQL, and semantics in MarkLogic see
- * <a href="https://docs.marklogic.com/guide/semantics" target="_top">Semantics Developer's Guide</a>
+ * <p>For details about RDF, SPARQL, and semantics in MarkLogic see the
+ * <a href="https://docs.marklogic.com/guide/semantics" target="_top">Semantics Developer's Guide</a>.
  */
 public interface SPARQLQueryManager  {
     /** Instantiate a new SPARQLQueryDefinition.  */
     public SPARQLQueryDefinition newQueryDefinition();
+
     /** Instantiate a new SPARQLQueryDefinition with provided SPARQL.  */
     public SPARQLQueryDefinition newQueryDefinition(String sparql);
+
     /** Instantiate a new SPARQLQueryDefinition with the SPARQL from
      * the provided TextWriteHandle.
      */
@@ -78,6 +97,7 @@ public interface SPARQLQueryManager  {
      * @return the results in the provided SPARQLResultsReadHandle
      */
     public <T extends SPARQLResultsReadHandle> T executeSelect(SPARQLQueryDefinition qdef, T handle);
+
     /** Execute a SPARQL "SELECT" query.
      * @param qdef the query
      * @param handle the handle capable of reading {@link SPARQLMimeTypes sparql results}
@@ -85,6 +105,7 @@ public interface SPARQLQueryManager  {
      * @return the results in the provided SPARQLResultsReadHandle
      */
     public <T extends SPARQLResultsReadHandle> T executeSelect(SPARQLQueryDefinition qdef, T handle, Transaction tx);
+
     /** Execute a SPARQL "SELECT" query.
      * @param qdef the query
      * @param handle the handle capable of reading {@link SPARQLMimeTypes sparql results}
@@ -93,6 +114,7 @@ public interface SPARQLQueryManager  {
      * @return the results in the provided SPARQLResultsReadHandle
      */
     public <T extends SPARQLResultsReadHandle> T executeSelect(SPARQLQueryDefinition qdef, T handle, long start);
+
     /** Execute a SPARQL "SELECT" query.
      * @param qdef the query
      * @param handle the handle capable of reading {@link SPARQLMimeTypes sparql results}
@@ -102,12 +124,15 @@ public interface SPARQLQueryManager  {
      * @return the results in the provided SPARQLResultsReadHandle
      */
     public <T extends SPARQLResultsReadHandle> T executeSelect(SPARQLQueryDefinition qdef, T handle, long start, Transaction tx);
+
     /** @return the currently set pageLength or -1 if no page length has been set */
     public long getPageLength();
+
     /** Set a page length for all SPARQL "SELECT" queries sent by this instance.
      * @param pageLength the non-negative number of results per page
      */
     public void setPageLength(long pageLength);
+
     /** Reset this instance to have no page length set. */
     public void clearPageLength();
 
@@ -117,18 +142,21 @@ public interface SPARQLQueryManager  {
      * @return the results in the provided TriplesReadHandle
      */
     public <T extends TriplesReadHandle> T executeConstruct(SPARQLQueryDefinition qdef, T handle);
+
     /** Execute a SPARQL "CONSTRUCT" statement.
      * @param qdef the SPARQL "CONSTRUCT" statement
      * @param handle the handle capable of reading {@link RDFMimeTypes triples or quads results}
      * @return the results in the provided TriplesReadHandle
      */
     public <T extends TriplesReadHandle> T executeConstruct(SPARQLQueryDefinition qdef, T handle, Transaction tx);
+
     /** Execute a SPARQL "DESCRIBE" query (which implements the Concise Bounded Description specification).
      * @param qdef the query
      * @param handle the handle capable of reading {@link RDFMimeTypes triples or quads results}
      * @return the results in the provided TriplesReadHandle
      */
     public <T extends TriplesReadHandle> T executeDescribe(SPARQLQueryDefinition qdef, T handle);
+
     /** Execute a SPARQL "DESCRIBE" query (which implements the Concise Bounded Description specification).
      * @param qdef the query
      * @param handle the handle capable of reading {@link RDFMimeTypes triples or quads results}
@@ -136,30 +164,50 @@ public interface SPARQLQueryManager  {
      * @return the results in the provided TriplesReadHandle
      */
     public <T extends TriplesReadHandle> T executeDescribe(SPARQLQueryDefinition qdef, T handle, Transaction tx);
+
     /** Execute a SPARQL "ASK" statement.
      * @param qdef the SPARQL "CONSTRUCT" statement
      * @return the answer as Boolean
      */
     public Boolean executeAsk(SPARQLQueryDefinition qdef);
+
     /** Execute a SPARQL "ASK" statement.
      * @param qdef the SPARQL "CONSTRUCT" statement
      * @return the answer as Boolean
      */
     public Boolean executeAsk(SPARQLQueryDefinition qdef, Transaction tx);
-    /** Execute a SPARQL update statement.
+
+    /** Execute a SPARQL update statement.  For an example of using with
+     * permisisons see {@link #permission}.
+     *
      * @param qdef the SPARQL update statement
      */
     public void executeUpdate(SPARQLQueryDefinition qdef);
-    /** Execute a SPARQL update statement.
+
+    /** Execute a SPARQL update statement.  For an example of using with
+     * permisisons see {@link #permission}.
+     *
      * @param qdef the SPARQL update statement
      * @param tx the multi-statement transaction
      */
     public void executeUpdate(SPARQLQueryDefinition qdef, Transaction tx);
 
-    /** Create a GraphPermissions builder object with the specified role and capabilities.
+
+    /** <p>For use with SPARQL update, where specified permissions will apply
+     * to any records created by the update.  Create a GraphPermissions builder
+     * object with the specified role and capabilities.</p>
+     *
+     * <p>For example:</p>
+     *
+     * <pre>    String sparqlUpdate = "INSERT DATA { &lt;a&gt; &lt;b&gt; &lt;c&gt; }";
+     *    SPARQLQueryDefinition qdef = sparqlMgr.newQueryDefinition(sparqlUpdate);
+     *    qdef.setUpdatePermissions(sparqlMgr.permission("rest-reader", Capability.UPDATE));
+     *    sparqlMgr.executeUpdate(qdef);</pre>
+     *
      * @param role the name of the role receiving these capabilities
      * @param capabilities the capabilities (read, update, or execute) granted to this role
      * @return the new GraphPermissions object with these capabilities set
      */
     public GraphPermissions permission(String role, Capability... capabilities);
+
 }
