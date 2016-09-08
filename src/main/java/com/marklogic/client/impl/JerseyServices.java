36870c800bcf24bd7edbc7530b16fac29bc6fa48
Sam Mefford <sam.mefford@marklogic.com>
2016-09-08 10:06:25 -0600

    revert a small change that broke compilation

--- src/main/java/com/marklogic/client/impl/JerseyServices.java
+++ src/main/java/com/marklogic/client/impl/JerseyServices.java
@@ -834,7 +834,7 @@ public class JerseyServices implements RESTServices {
 
 		JerseyDocumentPage(JerseyResultIterator iterator, boolean hasContent, boolean hasMetadata) {
 			super(
-				new ArrayList<T>().iterator(),
+				new ArrayList<DocumentRecord>().iterator(),
 				iterator != null ? iterator.getStart() : 1,
 				iterator != null ? iterator.getPageSize() : 0,
 				iterator != null ? iterator.getTotalSize() : 0
