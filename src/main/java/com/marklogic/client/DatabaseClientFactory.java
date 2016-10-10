marklogic/java-client-api

commit 72de2520ff08e91a4d5775c91fc09f08f1f13650

remove DatabaseClient.get* methods now encapsulated in getSecurityContext; revamp DatabaseClientFactory so it can return getSecurityContext so DMSDK classes no longer need to use deprecated newClient methods; change DataMovementManager into an interface; add DatabaseClient.newDataMovementManager to align with other *Manager classes; add HostAvailabilityListener to handle a failover scenario; add WriteEvent.getContent and getMetadata to enable HostAvailabilityListener to re-queue failed batches on failover; rename ForestConfiguration and Forest getHostName() to getHost() to align with DatabaseClient; remove Forest *FragmentCount and ForestConfiguration assign and getAssignmentPolicy since they're unused and unimplemented; demonstrate implementing a helpful ForestConfiguration called FilteredForestConfiguration; add WriteHostBatcher.add(WriteEvent...) to simplify re-queueing batches; clean up DataMovementEventImpl and WriteEvent to use covariant return types for method chaining; add DataMovementManager.stopJob(HostBatcher) for convenience when using JobTicket isn't necessary; add HostBatcher.withForestConfig so at run-time the forest config can be changed (mostly to add/remove hosts)--this is used by HostAvailabilityListener to black-list failed hosts; hide getForestClient in DataMovementManagerImpl since it isn't needed by application developers and the name is misleading since it's really just a client pointed to another host

src/main/java/com/marklogic/client/DatabaseClientFactory.java
@@ -557,7 +557,6 @@ public class DatabaseClientFactory {
 					sslVerifier = SSLHostnameVerifier.COMMON;
 				}
 			}
-			return newClient(host, port, database, user, password, type, sslContext, sslVerifier);
 		} else if (securityContext instanceof DigestAuthContext) {
 			DigestAuthContext digestContext = (DigestAuthContext) securityContext;
 			user = digestContext.user;
@@ -571,9 +570,9 @@ public class DatabaseClientFactory {
 					sslVerifier = SSLHostnameVerifier.COMMON;
 				}
 			}
-			return newClient(host, port, database, user, password, type, sslContext, sslVerifier);
 		} else if (securityContext instanceof KerberosAuthContext) {
 			KerberosAuthContext kerberosContext = (KerberosAuthContext) securityContext;
+			user = kerberosContext.externalName;
 			type = Authentication.KERBEROS;
 			if (kerberosContext.sslContext != null) {
 				sslContext = kerberosContext.sslContext;
@@ -583,19 +582,49 @@ public class DatabaseClientFactory {
 					sslVerifier = SSLHostnameVerifier.COMMON;
 				}
 			}
-			return newClient(host, port, database, kerberosContext.externalName, null, type, sslContext, sslVerifier);
 		} else if (securityContext instanceof CertificateAuthContext) {
 			CertificateAuthContext certificateContext = (CertificateAuthContext) securityContext;
 			type = Authentication.CERTIFICATE;
+			sslContext = certificateContext.getSSLContext();
 			if (certificateContext.sslVerifier != null) {
 				sslVerifier = certificateContext.sslVerifier;
 			} else {
 				sslVerifier = SSLHostnameVerifier.COMMON;
 			}
-			return newClient(host, port, database, null, null, type, certificateContext.getSSLContext(), sslVerifier);
+		} else {
+			throw new IllegalArgumentException("securityContext must be of type BasicAuthContext, " +
+				"DigestAuthContext, KerberosAuthContext, or CertificateAuthContext");
 		}
-		return null;
+
+		JerseyServices services = new JerseyServices();
+		services.connect(host, port, database, user, password, type, sslContext, sslVerifier);
+
+		if (clientConfigurator != null) {
+			((HttpClientConfigurator) clientConfigurator).configure(
+				services.getClientImplementation()
+				);
+		}
+
+		DatabaseClientImpl client = new DatabaseClientImpl(services, host, port, database, securityContext);
+		client.setHandleRegistry(getHandleRegistry().copy());
+		return client;
 	}
+
+
+	static private SecurityContext makeSecurityContext(String user, String password, Authentication type, SSLContext context, SSLHostnameVerifier verifier) {
+		if ( Authentication.BASIC == type ) {
+			return new BasicAuthContext(user, password)
+				.withSSLContext(context)
+				.withSSLHostnameVerifier(verifier);
+		} else if ( Authentication.DIGEST == type ) {
+			return new DigestAuthContext(user, password)
+				.withSSLContext(context)
+				.withSSLHostnameVerifier(verifier);
+		} else {
+			throw new IllegalStateException("makeSecurityContext should only be called with BASIC or DIGEST Authentication");
+		}
+	}
+
 	/**
 	 * Creates a client to access the database by means of a REST server.
 	 * 
@@ -609,7 +638,7 @@ public class DatabaseClientFactory {
 	 */
 	@Deprecated
 	static public DatabaseClient newClient(String host, int port, String user, String password, Authentication type) {
-		return newClient(host, port, null, user, password, type, null, null);
+		return newClient(host, port, null, makeSecurityContext(user, password, type, null, null));
 	}
 	/**
 	 * Creates a client to access the database by means of a REST server.
@@ -625,7 +654,7 @@ public class DatabaseClientFactory {
 	 */
 	@Deprecated
 	static public DatabaseClient newClient(String host, int port, String database, String user, String password, Authentication type) {
-		return newClient(host, port, database, user, password, type, null, null);
+		return newClient(host, port, database, makeSecurityContext(user, password, type, null, null));
 	}
 	/**
 	 * Creates a client to access the database by means of a REST server.
@@ -641,7 +670,7 @@ public class DatabaseClientFactory {
 	 */
 	@Deprecated
 	static public DatabaseClient newClient(String host, int port, String user, String password, Authentication type, SSLContext context) {
-		return newClient(host, port, null, user, password, type, context, SSLHostnameVerifier.COMMON);
+		return newClient(host, port, null, makeSecurityContext(user, password, type, context, SSLHostnameVerifier.COMMON));
 	}
 	/**
 	 * Creates a client to access the database by means of a REST server.
@@ -658,7 +687,7 @@ public class DatabaseClientFactory {
 	 */
 	@Deprecated
 	static public DatabaseClient newClient(String host, int port, String database, String user, String password, Authentication type, SSLContext context) {
-		return newClient(host, port, database, user, password, type, context, SSLHostnameVerifier.COMMON);
+		return newClient(host, port, database, makeSecurityContext(user, password, type, context, SSLHostnameVerifier.COMMON));
 	}
 	/**
 	 * Creates a client to access the database by means of a REST server.
@@ -675,9 +704,7 @@ public class DatabaseClientFactory {
 	 */
 	@Deprecated
 	static public DatabaseClient newClient(String host, int port, String user, String password, Authentication type, SSLContext context, SSLHostnameVerifier verifier) {
-		DatabaseClientImpl client = newClientImpl(host, port, null, user, password, type, context, verifier);
-		client.setHandleRegistry(getHandleRegistry().copy());
-		return client;
+		return newClient(host, port, null, makeSecurityContext(user, password, type, context, verifier));
 	}
 	/**
 	 * Creates a client to access the database by means of a REST server.
@@ -695,23 +722,7 @@ public class DatabaseClientFactory {
 	 */
 	@Deprecated
 	static public DatabaseClient newClient(String host, int port, String database, String user, String password, Authentication type, SSLContext context, SSLHostnameVerifier verifier) {
-		DatabaseClientImpl client = newClientImpl(host, port, database, user, password, type, context, verifier);
-		client.setHandleRegistry(getHandleRegistry().copy());
-		return client;
-	}
-
-	static private DatabaseClientImpl newClientImpl(String host, int port, String database, String user, String password, Authentication type, SSLContext context, SSLHostnameVerifier verifier) {
-		logger.debug("Creating new database client for server at "+host+":"+port);
-		JerseyServices services = new JerseyServices();
-		services.connect(host, port, database, user, password, type, context, verifier);
-
-		if (clientConfigurator != null) {
-			((HttpClientConfigurator) clientConfigurator).configure(
-				services.getClientImplementation()
-				);
-		}
-
-		return new DatabaseClientImpl(services, host, port, database, user, password, type, context, verifier);
+		return newClient(host, port, database, makeSecurityContext(user, password, type, context, verifier));
 	}
 
 	/**
@@ -1020,10 +1031,8 @@ public class DatabaseClientFactory {
 		 * @return	a new client for making database requests
 		 */
 		public DatabaseClient newClient() {
-			DatabaseClientImpl client = newClientImpl(host, port, database, user, password, authentication, context, verifier);
-			client.setHandleRegistry(getHandleRegistry().copy());
-
-			return client;
+			return DatabaseClientFactory.newClient(host, port, database,
+				makeSecurityContext(user, password, authentication, context, verifier));
 		}
 	}
 }

src/main/java/com/marklogic/client/DatabaseClient.java
@@ -19,6 +19,7 @@ import java.io.OutputStream;
 import java.io.Serializable;
 
 import com.marklogic.client.admin.ServerConfigurationManager;
+import com.marklogic.client.datamovement.DataMovementManager;
 import com.marklogic.client.document.BinaryDocumentManager;
 import com.marklogic.client.document.GenericDocumentManager;
 import com.marklogic.client.document.JSONDocumentManager;
@@ -97,6 +98,13 @@ public interface DatabaseClient {
      * @return	a manager supporting operations on XMLdocuments
      */
     XMLDocumentManager newXMLDocumentManager();
+
+    /**
+     * Creates a manager for long-running asynchronous write or query jobs.  Don't forget
+     * to call dataMovementManager.release() when you're done with it.
+     * @return	a manager supporting long-running asynchronous write or query jobs
+     */
+    DataMovementManager newDataMovementManager();
 
     /**
      * Creates a manager to query for database documents.
@@ -209,16 +217,6 @@ public interface DatabaseClient {
     int getPort();
 
     String getDatabase();
-
-    String getUser();
-
-    String getPassword();
-
-    Authentication getAuthentication();
-
-    SSLContext getSSLContext();
-
-    SSLHostnameVerifier getSSLHostnameVerifier();
 
     SecurityContext getSecurityContext();
 }

src/main/java/com/marklogic/client/datamovement/DataMovementManager.java
@@ -17,45 +17,43 @@ package com.marklogic.client.datamovement;
 
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.query.QueryDefinition;
-import com.marklogic.client.datamovement.impl.QueryHostBatcherImpl;
-import com.marklogic.client.datamovement.impl.DataMovementServices;
-import com.marklogic.client.datamovement.impl.WriteHostBatcherImpl;
+import com.marklogic.client.datamovement.impl.DataMovementManagerImpl;
 
 import java.util.Iterator;
 
 /**
  * DataMovementManager is the starting point for getting new instances of
  * QueryHostBatcher and WriteHostBatcher, configured with a DatabaseClient and
- * ForestConfiguration.  The first thing must always be to call {@link
- * #withClient withClient} which will provide a DatabaseClient with which it
- * can obtain the ForestConfiguration and from which it can create host-specific
- * DatabaseClient instances for each applicable host.  Applicable hosts are
- * those with forests for the database specified in the DatabaseClient.  If not
- * specified, the database is the default database for the port in the
- * DatabaseClient.
+ * ForestConfiguration.  On instantiation, it will immediately call
+ * readForestConfig to obtain the ForestConfiguration from which it can create
+ * host-specific DatabaseClient instances for each applicable host.  Applicable
+ * hosts are those with forests for the database specified in the
+ * DatabaseClient.  If not specified, the database is the default database for
+ * the port in the DatabaseClient.  Call {@link release()
+ * dataMovementMangaer.release()} when you're done with your
+ * DataMovementManager instance to free resources associated with those
+ * host-specific DatabaseClient instances.
  *
  * Sample Usage:
  *
- *     DataMovementManager dataMovementManager = DataMovementManager.newInstance()
- *         .withClient(client);
+ *     DataMovementManager dataMovementManager = databaseClient.newDataMovementManager();
+ *     WriteHostBatcher batcher = dataMovementManager.newWriteHostBatcher();
+ *     dataMovementManager.startJob(batcher);
+ *     . . .
+ *     dataMovementManager.stopJob(batcher);
+ *     dataMovementManager.release();
  */
-public class DataMovementManager {
-  private DatabaseClient client;
-  private DataMovementServices service = new DataMovementServices();
-  private ForestConfiguration forestConfig;
-
-  private DataMovementManager() {
-    // TODO: implement
-  }
-
+public interface DataMovementManager {
   /**
    * Instantiate a new instance.  Generally only one DataMovementManager
    * instance is needed per database.
    *
    * @return a new DataMovementManager instance
+   * @deprecated use databaseClient.newDataMovementManager() instead
    */
+  @Deprecated
   public static DataMovementManager newInstance() {
-    return new DataMovementManager();
+    return new DataMovementManagerImpl();
   }
 
   /**
@@ -66,20 +64,22 @@ public class DataMovementManager {
    * @param client the DatabaseClient instance
    * @return this instance for method chaining
    */
-  public DataMovementManager withClient(DatabaseClient client) {
-    this.client = client;
-    service.setClient(client);
-    return this;
-  }
+  @Deprecated
+  public DataMovementManager withClient(DatabaseClient client);
 
   /**
    * get the current DatabaseClient
    *
    * @return the current DatabaseClient
    */
-  public DatabaseClient getClient() {
-    return client;
-  }
+  @Deprecated
+  public DatabaseClient getClient();
+
+  /** Calls release() on all host-specific DatabaseClient instances (but not on
+   * the DatabaseClient instance used to create this DataMovementManager
+   * instance).
+   */
+  public void release();
 
   /**
    * Begins job tracking on the WriteHostBatcher.  Calling startJob is not
@@ -88,27 +88,21 @@ public class DataMovementManager {
    * @param batcher the WriteHostBatcher instance which has to be started
    * @return a JobTicket which can be used to track the job
    */
-  public JobTicket startJob(WriteHostBatcher batcher) {
-    return service.startJob(batcher);
-  }
+  public JobTicket startJob(WriteHostBatcher batcher);
 
   /**
    * Starts the QueryHostBatcher job.
    * @param batcher the QueryHostBatcher instance which has to be started
    * @return a JobTicket which can be used to track the job
    */
-  public JobTicket startJob(QueryHostBatcher batcher) {
-    return service.startJob(batcher);
-  }
+  public JobTicket startJob(QueryHostBatcher batcher);
 
   /**
    * Not yet implemented 
    * @param ticket the JobTicket for which the report has to be generated
    * @return the report for the job
    */
-  public JobReport getJobReport(JobTicket ticket) {
-    return service.getJobReport(ticket);
-  }
+  public JobReport getJobReport(JobTicket ticket);
 
   /**
    * Immediately cancel all queued tasks, prevent new tasks from being added
@@ -117,21 +111,23 @@ public class DataMovementManager {
    * mechanism.
    * @param ticket indicates the job which has to be stopped
    */
-  public void stopJob(JobTicket ticket) {
-    service.stopJob(ticket);
-  }
+  public void stopJob(JobTicket ticket);
+
+  /**
+   * Immediately cancel all queued tasks, prevent new tasks from being added
+   * to the queue, and free all resources.  In-process tasks cannot be
+   * cancelled because they talk to the REST server which does not have such a
+   * mechanism.
+   * @param batcher the batcher instance to stop
+   */
+  public void stopJob(HostBatcher batcher);
 
   /**
    * Create a new WriteHostBatcher instance.
    *
    * @return the new WriteHostBatcher instance
    */
-  public WriteHostBatcher newWriteHostBatcher() {
-    verifyClientIsSet("newWriteHostBatcher");
-    WriteHostBatcherImpl batcher = new WriteHostBatcherImpl(getForestConfig());
-    if ( client != null ) batcher.setClient(client);
-    return batcher;
-  }
+  public WriteHostBatcher newWriteHostBatcher();
 
   /**
    * Create a new QueryHostBatcher instance configured to retrieve uris that
@@ -144,12 +140,7 @@ public class DataMovementManager {
    *
    * @return the new QueryHostBatcher instance
    */
-  public QueryHostBatcher newQueryHostBatcher(QueryDefinition query) {
-    verifyClientIsSet("newQueryHostBatcher");
-    QueryHostBatcherImpl batcher = new QueryHostBatcherImpl(query, getForestConfig());
-    if ( client != null ) batcher.setClient(client);
-    return batcher;
-  }
+  public QueryHostBatcher newQueryHostBatcher(QueryDefinition query);
 
   /**
    * Create a new QueryHostBatcher instance configured to retrieve uris from
@@ -163,31 +154,12 @@ public class DataMovementManager {
    *
    * @return the new QueryHostBatcher instance
    */
-  public QueryHostBatcher newQueryHostBatcher(Iterator<String> iterator) {
-    verifyClientIsSet("newQueryHostBatcher");
-    QueryHostBatcherImpl batcher = new QueryHostBatcherImpl(iterator, getForestConfig());
-    if ( client != null ) batcher.setClient(client);
-    return batcher;
-  }
-
-  private ForestConfiguration getForestConfig() {
-    if ( forestConfig != null ) return forestConfig;
-    return readForestConfig();
-  }
+  public QueryHostBatcher newQueryHostBatcher(Iterator<String> iterator);
 
   /**
    * Update the ForestConfiguration with the latest from the server.
    *
    * @return the latest ForestConfiguration from the server
    */
-  public ForestConfiguration readForestConfig() {
-    verifyClientIsSet("readForestConfig");
-    forestConfig = service.readForestConfig();
-    return forestConfig;
-  }
-
-  private void verifyClientIsSet(String method) {
-    if ( client == null ) throw new IllegalStateException("The method " + method +
-      " cannot be called without first calling withClient()");
-  }
+  public ForestConfiguration readForestConfig();
 }

src/main/java/com/marklogic/client/datamovement/FilteredForestConfiguration.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright 2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.client.datamovement;
+
+import com.marklogic.client.datamovement.impl.ForestImpl;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class FilteredForestConfiguration implements ForestConfiguration {
+  ForestConfiguration wrappedForestConfig;
+  Set<String> blackList = new HashSet<>();
+  Set<String> whiteList = new HashSet<>();
+  Map<String,String> renames = new HashMap<>();
+
+  FilteredForestConfiguration(ForestConfiguration forestConfig) {
+    this.wrappedForestConfig = forestConfig;
+  }
+
+  public Forest[] listForests() {
+    Stream<Forest> stream = Stream.of(wrappedForestConfig.listForests());
+    if ( blackList.size() > 0 ) {
+      stream = stream.filter((forest) -> ! blackList.contains(forest.getHost()));
+    } else if ( whiteList.size() > 0 ) {
+      stream = stream.filter((forest) -> whiteList.contains(forest.getHost()));
+    }
+    // apply renames
+    stream = stream.map((forest) -> {
+      if ( renames.containsKey(forest.getHost()) ) {
+        ((ForestImpl) forest).setHost(renames.get(forest.getHost()));
+      }
+      return forest;
+    });
+    return stream.toArray(Forest[]::new);
+  }
+
+  public FilteredForestConfiguration withBlackList(String... hostNames) {
+    if ( whiteList.size() > 0 ) throw new IllegalStateException("whiteList already initialized");
+    for ( String hostName : hostNames ) {
+      blackList.add(hostName);
+    }
+    return this;
+  }
+
+  public FilteredForestConfiguration withWhiteList(String... hostNames) {
+    if ( blackList.size() > 0 ) throw new IllegalStateException("blackList already initialized");
+    for ( String hostName : hostNames ) {
+      whiteList.add(hostName);
+    }
+    return this;
+  }
+
+  public FilteredForestConfiguration withRenamedHost(String hostName, String targetHostName) {
+    renames.put(hostName, targetHostName);
+    return this;
+  }
+}

src/main/java/com/marklogic/client/datamovement/Forest.java
@@ -16,13 +16,10 @@
 package com.marklogic.client.datamovement;
 
 public interface Forest {
-  String getHostName();
+  String getHost();
   String getDatabaseName();
   String getForestName();
   String getForestId();
   boolean isUpdateable();
   boolean isDeleteOnly();
-  long getFragmentCount();
-  void increaseFragmentCount(long numFragments);
-  void decreaseFragmentCount(long numFragments);
 }

src/main/java/com/marklogic/client/datamovement/ForestConfiguration.java
@@ -17,11 +17,12 @@ package com.marklogic.client.datamovement;
 
 import com.marklogic.client.DatabaseClient;
 
+import java.util.stream.Stream;
+
 public interface ForestConfiguration {
   Forest[] listForests();
-  Forest assign(String uri);
-  DatabaseClient getForestClient(Forest forest);
-  AssignmentPolicy getAssignmentPolicy();
 
-  enum AssignmentPolicy { BUCKET, LEGACY, STATISTICAL, RANGE };
+  public default String[] getHosts() {
+    return Stream.of(listForests()).map((forest) -> forest.getHost()).toArray(String[]::new);
+  }
 }

src/main/java/com/marklogic/client/datamovement/HostAvailabilityListener.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright 2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.client.datamovement;
+
+import com.marklogic.client.DatabaseClient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.http.NoHttpResponseException;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import javax.net.ssl.SSLException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class HostAvailabilityListener implements FailureListener<QueryHostException>, BatchFailureListener<WriteEvent> {
+  private static Logger logger = LoggerFactory.getLogger(HostAvailabilityListener.class);
+  private DataMovementManager moveMgr;
+  private HostBatcher batcher;
+  private Duration suspendTimeForHostUnavailable = Duration.ofMinutes(1);
+  private int minHosts = 1;
+  private ScheduledFuture<?> future;
+  List<Class<?>> hostUnavailableExceptions = new ArrayList<>();
+  {
+    hostUnavailableExceptions.add(NoHttpResponseException.class);
+    hostUnavailableExceptions.add(SocketException.class);
+    hostUnavailableExceptions.add(SSLException.class);
+    hostUnavailableExceptions.add(UnknownHostException.class);
+  }
+
+  public HostAvailabilityListener(DataMovementManager moveMgr, HostBatcher batcher) {
+    if (moveMgr == null) throw new IllegalArgumentException("moveMgr must not be null");
+    if (batcher == null) throw new IllegalArgumentException("batcher must not be null");
+    this.moveMgr = moveMgr;
+    this.batcher = batcher;
+  }
+
+  /** If a host becomes unavailable (NoHttpResponseException, SocketException, SSLException,
+   * UnknownHostException), adds it to the blacklist
+   */
+  public HostAvailabilityListener withSuspendTimeForHostUnavailable(Duration
+  duration) {
+    if (duration == null) throw new IllegalArgumentException("duration must not be null");
+    this.suspendTimeForHostUnavailable = duration;
+    return this;
+  }
+
+  /** If less than minHosts are left, calls stopJob.
+   */
+  public HostAvailabilityListener withMinHosts(int numHosts) {
+    this.minHosts = numHosts;
+    return this;
+  }
+
+  /** Overwrites the list of exceptions for which a host will be blacklisted
+   */
+  public HostAvailabilityListener withHostUnavailableExceptions(Class<Throwable>... exceptionTypes) {
+    hostUnavailableExceptions = new ArrayList<>();
+    for ( Class<Throwable> exception : exceptionTypes ) {
+      hostUnavailableExceptions.add(exception);
+    }
+    return this;
+  }
+
+  public Throwable[] getHostUnavailableExceptions() {
+    return hostUnavailableExceptions.toArray(new Throwable[hostUnavailableExceptions.size()]);
+  }
+
+  public Duration getSuspendTimeForHostUnavailable() {
+    return suspendTimeForHostUnavailable;
+  }
+
+  public int getMinHosts() {
+    return minHosts;
+  }
+
+  public void processEvent(DatabaseClient hostClient, Batch<WriteEvent> batch, Throwable throwable) {
+    processException(throwable, hostClient.getHost());
+  }
+
+  public void processFailure(DatabaseClient client, QueryHostException throwable) {
+    processException(throwable, client.getHost());
+  }
+
+  public void processException(Throwable throwable, String host) {
+    if ( isHostUnavailableException(throwable, new HashSet<>()) ) {
+      ForestConfiguration existingForestConfig = batcher.getForestConfig();
+      if ( minHosts < existingForestConfig.getHosts().length ) {
+        logger.error("ERROR: host unavailable \"" + host + "\", black-listing it for " +
+          suspendTimeForHostUnavailable.toString(), throwable);
+        ForestConfiguration filteredForestConfig =
+          new FilteredForestConfiguration(existingForestConfig)
+            .withBlackList(host);
+        batcher.withForestConfig(filteredForestConfig);
+        // cancel any previously scheduled re-sync
+        if ( future != null ) future.cancel(false);
+        // schedule a re-sync with the server forest config
+        future = Executors.newScheduledThreadPool(1)
+          .schedule( () -> {
+              ForestConfiguration currentForestConfig = moveMgr.readForestConfig();
+              // set the forestConfig back to whatever the server says it is
+              batcher.withForestConfig(currentForestConfig);
+              String hosts = Stream.of(currentForestConfig.listForests())
+                .map((forest) -> forest.getHost())
+                .distinct()
+                .collect(Collectors.joining(", "));
+              logger.info("it's been {} since host {} failed, opening communication to all server hosts [{}]",
+                suspendTimeForHostUnavailable.toString(), host, hosts);
+            }
+            , suspendTimeForHostUnavailable.toMillis(), TimeUnit.MILLISECONDS);
+      } else {
+        // by black-listing this host we'd move below minHosts, so it's time to
+        // stop this job
+        logger.error("Encountered [" + throwable + "] on host \"" + host +
+          "\" but black-listing it would drop job below minHosts (" + minHosts +
+          "), so stopping job \"" + batcher.getJobName() + "\"", throwable);
+        moveMgr.stopJob(batcher);
+      }
+    }
+  }
+
+  private boolean isHostUnavailableException(Throwable throwable, Set<Throwable> path) {
+    for ( Class<?> type : hostUnavailableExceptions ) {
+      if ( type.isInstance(throwable) ) {
+        return true;
+      }
+    }
+    // we need to check our recursion path to avoid infinite recursion if a
+    // getCause() pointed to itself or an ancestor
+    if ( throwable.getCause() != null && ! path.contains(throwable.getCause()) ) {
+      path.add(throwable.getCause());
+      boolean isCauseHostUnavailableException = isHostUnavailableException(throwable.getCause(), path);
+      if ( isCauseHostUnavailableException == true ) return true;
+    }
+    return false;
+  }
+}

src/main/java/com/marklogic/client/datamovement/HostBatcher.java
@@ -22,4 +22,6 @@ public interface HostBatcher {
   int getBatchSize();
   HostBatcher withThreadCount(int threadCount);
   int getThreadCount();
+  ForestConfiguration getForestConfig();
+  HostBatcher withForestConfig(ForestConfiguration forestConfig);
 }

commit 54ce612b4711fab7107b0ba863f6f61ccf0e154c

rollback a bug introduced in 72de252 which broke HandleAsTest--Bean has a separate default HandleRegistry for some reason

src/main/java/com/marklogic/client/DatabaseClientFactory.java
@@ -1031,8 +1031,10 @@ public class DatabaseClientFactory {
 		 * @return	a new client for making database requests
 		 */
 		public DatabaseClient newClient() {
-			return DatabaseClientFactory.newClient(host, port, database,
+			DatabaseClientImpl client = (DatabaseClientImpl) DatabaseClientFactory.newClient(host, port, database,
 				makeSecurityContext(user, password, authentication, context, verifier));
+			client.setHandleRegistry(getHandleRegistry().copy());
+			return client;
 		}
 	}
 }