3d40acdd7751b522f45c71dfa8b4eb8f74fad85d
vivekmuniyandi <vivek.siddharthan@gmail.com>
2016-10-21 16:34:39 -0700

    https://github.com/marklogic/data-movement/issues/135 - Write and Read POJO cookbook example

---
+++ src/main/java/com/marklogic/client/example/cookbook/datamovement/DatabaseClientSingleton.java
@@ -0,0 +1,39 @@
+package com.marklogic.client.example.cookbook.datamovement;
+
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.DatabaseClientFactory.DigestAuthContext;
+import com.marklogic.client.example.cookbook.Util;
+import com.marklogic.client.example.cookbook.Util.ExampleProperties;
+import com.marklogic.client.io.JAXBHandle;
+
+public class DatabaseClientSingleton {
+  private static DatabaseClient client = null;
+  
+  private DatabaseClientSingleton() {}
+
+  public static void register(Class<?>...pojoClasses) throws JAXBException {
+    if(client != null) {
+      throw new IllegalStateException("Cannot register classes after client instance is created!");
+    }
+    DatabaseClientFactory.getHandleRegistry().register(JAXBHandle.newFactory(pojoClasses));
+  }
+
+  public static DatabaseClient get() {
+    if(client == null) {
+      ExampleProperties properties = null;
+      try {
+        properties = Util.loadProperties();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      client = DatabaseClientFactory.newClient(properties.host, properties.port,
+          new DigestAuthContext(properties.writerUser, properties.writerPassword));
+    }
+    return client;
+  }
+}
---
+++ src/main/java/com/marklogic/client/example/cookbook/datamovement/WriteandReadPOJOs.java
@@ -0,0 +1,197 @@
+package com.marklogic.client.example.cookbook.datamovement;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.document.DocumentPage;
+import com.marklogic.client.document.DocumentRecord;
+import com.marklogic.client.document.XMLDocumentManager;
+import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.query.QueryDefinition;
+import com.marklogic.client.query.StructuredQueryBuilder;
+import com.marklogic.client.datamovement.DataMovementManager;
+import com.marklogic.client.datamovement.DeleteListener;
+import com.marklogic.client.datamovement.ExportListener;
+import com.marklogic.client.datamovement.QueryHostBatcher;
+import com.marklogic.client.datamovement.WriteHostBatcher;
+
+/**
+ * WriteandReadPOJOS illustrates how to write bulk POJOs into the database
+ * using WriteHostBatcher. It also demonstrates how to read POJOs from the
+ * database using QueryHostBatcher with and without ExportListener
+ */
+public class WriteandReadPOJOs {
+  private int batchSize = 10;
+  private int threadCount = 5;
+  private Logger logger = LoggerFactory.getLogger(WriteandReadPOJOs.class);
+  private DatabaseClient client = null;
+  private DataMovementManager moveMgr = null;
+
+  public static void main(String args[]) throws JAXBException {
+    new WriteandReadPOJOs().run();
+  }
+
+  /**
+   * A sample class which we will be using in order to demonstrate
+   * reading and writing large number of POJOs 
+   */
+  @XmlRootElement
+  public static class ProductDetails {
+    private int id;
+    private String name;
+    private int distributor_id;
+    public int getId() {
+      return id;
+    }
+    public void setId(int id) {
+      this.id = id;
+    }
+    public String getName() {
+      return name;
+    }
+    public void setName(String name) {
+      this.name = name;
+    }
+    public int getDistributor_id() {
+      return distributor_id;
+    }
+    public void setDistributor_id(int distributor_id) {
+      this.distributor_id = distributor_id;
+    }
+  }
+
+  public void run() throws JAXBException {
+    // Register the ProductDetails class so that it can be handled by the JAXBHandle
+    DatabaseClientSingleton.register(ProductDetails.class);
+    // Create the Database Client and Data Movement Manager
+    client =  DatabaseClientSingleton.get();
+    moveMgr = client.newDataMovementManager();
+    writeBulkPOJOS();
+    readBulkPOJOS();
+    readPOJOsWithExportListener();
+    deleteDocuments();
+    moveMgr.release();
+  }
+
+  public void writeBulkPOJOS() throws JAXBException {
+    WriteHostBatcher batcher = moveMgr.newWriteHostBatcher()
+        // normally a batch would be at least 100 docs at a time. We're
+        // making this number small in order to demonstrate this example.
+        .withBatchSize(batchSize)
+        // the number of threads writing depends on how fast you have documents available
+        // and how many nodes are in your MarkLogic cluster
+        .withThreadCount(threadCount)
+        // create an action you want to do once a batch is successfully
+        // written here. It will be called for each successful batch.
+        .onBatchSuccess((dbclient, batch) -> {
+          logger.info("Batch {} wrote, {} so far", batch.getJobBatchNumber(), batch.getJobResultsSoFar());
+        })
+        // create an action you want to do when the batch fails. It will
+        // be called for every batch that fails.
+        .onBatchFailure((client, batch, throwable) -> throwable.printStackTrace() );
+    moveMgr.startJob(batcher);
+
+    for(int i=0;i<50;i++) {
+      // create instances of the POJO class
+      ProductDetails product = new ProductDetails();
+      product.setName("Product"+i);
+      product.setId(i);
+      product.setDistributor_id(100);
+
+      // Create metadata for the documents that needs to be ingested
+      DocumentMetadataHandle metadataHandle = new DocumentMetadataHandle();
+      if(i<25) {
+        metadataHandle.getCollections().add("products-collection1");
+      } else {
+        metadataHandle.getCollections().add("products-collection2");
+      }
+
+      String docId = "/dmsdkexample/"+product.getName()+".xml";
+      // Write the instance of the POJO class into the database
+      // along with the metadata created above
+      batcher.addAs(docId, metadataHandle, product);
+      
+    }
+    // any docs in an incomplete batch need to be written
+    batcher.flushAndWait();
+    moveMgr.stopJob(batcher);
+  }
+
+  public void readBulkPOJOS() throws JAXBException {
+    // Create a query definition in order to use it with QueryHostBatcher
+    QueryDefinition query = new StructuredQueryBuilder().collection("products-collection1");
+
+    // Create a QueryHostBatcher in order to retrieve bulk POJOs 
+    // from the database matching the query definition
+    QueryHostBatcher queryBatcher = moveMgr.newQueryHostBatcher(query)
+        .withBatchSize(batchSize)
+        .withThreadCount(threadCount)
+        // You can configure to do some action whenever a batch 
+        // of uris are retrieved and ready to process
+        .onUrisReady(
+          (client, batch) -> {
+            // Read the URIs and display them
+            XMLDocumentManager docMgr = client.newXMLDocumentManager();
+            DocumentPage documents = docMgr.read(batch.getItems());
+            for ( DocumentRecord record : documents ) {
+              ProductDetails product = record.getContentAs(ProductDetails.class);
+              System.out.println("Product ID : "+ product.getId());
+            }
+            
+          }
+        )
+        // Add a listener to do some action when the batch fails to retrieve
+        .onQueryFailure((client, throwable) -> throwable.printStackTrace());
+    
+    moveMgr.startJob(queryBatcher);
+    // Wait till the batch completes
+    queryBatcher.awaitCompletion();
+    moveMgr.stopJob(queryBatcher);
+  }
+
+  public void readPOJOsWithExportListener() throws JAXBException {
+    QueryDefinition query = new StructuredQueryBuilder().collection("products-collection2");
+
+    // Create a QueryHostBatcher to get the documents from the database
+    // and process each document retrieved using the Export Listener and
+    // all documents are exported at a consistent point-in-time using
+    // withConsistentSnapshot.
+    QueryHostBatcher exportBatcher = moveMgr.newQueryHostBatcher(query)
+        .withBatchSize(batchSize)
+        .withThreadCount(threadCount)
+        .onUrisReady(
+          new ExportListener()
+          .withConsistentSnapshot()
+          // Configure a listener to do some action for each document
+          // in the batch when each batch is ready
+          .onDocumentReady(
+            doc -> {
+              System.out.println("Product ID with Export Listener : "+ doc.getContentAs(ProductDetails.class).getId());
+            }
+          )
+        )
+        .onQueryFailure((client, throwable) -> throwable.printStackTrace());
+    
+    moveMgr.startJob(exportBatcher);
+    exportBatcher.awaitCompletion();
+    moveMgr.stopJob(exportBatcher);
+  }
+
+  public void deleteDocuments() {
+    QueryHostBatcher queryBatcher = moveMgr.newQueryHostBatcher(
+        new StructuredQueryBuilder().collection("products-collection1", "products-collection2")
+      )
+      .withBatchSize(batchSize)
+      .onUrisReady(new DeleteListener()) // Sends a bulk delete for all the documents in each batch
+      .withConsistentSnapshot()
+      .onQueryFailure((client, throwable) -> throwable.printStackTrace());
+
+    moveMgr.startJob(queryBatcher);
+    queryBatcher.awaitCompletion();
+    moveMgr.stopJob(queryBatcher);
+  }
+}
