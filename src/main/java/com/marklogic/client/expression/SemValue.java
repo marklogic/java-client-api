d90761bd01316d6bf7e97de1b2abc01acf9a3b77
ehennum <ehennum@marklogic.com>
2016-11-14 20:25:09 -0500

    JavaDoc for added store constructors in SemValue

--- src/main/java/com/marklogic/client/expression/SemValue.java
+++ src/main/java/com/marklogic/client/expression/SemValue.java
@@ -49,11 +49,48 @@ public interface SemValue {
 	 */
     public SemIriSeqVal iris(SemIriVal... iris);
 
+    /**
+     * Specifies a store for accessing triples, modifying the triple retrieval based on the options.
+     * @param options	the specification for tuning the retrieval of triples
+     * @return	the specification of the store with the triples
+     */
     public SemStoreExpr store(String... options);
+    /**
+     * Specifies a store for accessing triples, modifying the triple retrieval based on the options
+     * with a restriction to the triples contained in documents matching the query.
+     * @param options	the specification for tuning the retrieval of triples
+     * @param query	the CtsQuery matching the documents that contain the triples
+     * @return	the specification of the store with the triples
+     */
     public SemStoreExpr store(XsStringSeqVal options, CtsQueryExpr query);
+    /**
+     * Specifies a store with additional triples inferred based on rules.
+     * @param locations	specifies the rulesets for inferring additional triples
+     * @return	the specification of the store with the rulesets for inferring triples
+     */
     public SemStoreExpr rulesetStore(String... locations);
+    /**
+     * Specifies a store with additional triples inferred based on rules and
+     * a base store providing triples as input for the rules.
+     * @param locations	specifies the rulesets for inferring additional triples
+     * @param stores	specifies the stores providing triples as input for inferrence
+     * @return	the specification of the store with the rulesets for inferring triples
+     */
     public SemStoreExpr rulesetStore(XsStringSeqVal locations, SemStoreExpr... stores);
+    /**
+     * Specifies a store with additional triples inferred based on rules and
+     * a base store providing triples as input for the rules.
+     * @param locations	specifies the rulesets for inferring additional triples
+     * @param stores	specifies the stores providing triples as input for inferrence
+     * @param options	the specification for tuning the inference of new triples
+     * @return	the specification of the store with the rulesets for inferring triples
+     */
     public SemStoreExpr rulesetStore(XsStringSeqVal locations, SemStoreSeqExpr stores, String... options);
 
+    /**
+	 * Takes any number of triple store specifications and constructs a store sequence
+     * @param stores	the triple store specifications
+	 * @return	a sequence of triple store specifications
+     */
     public SemStoreSeqExpr stores(SemStoreExpr... stores);
 }
