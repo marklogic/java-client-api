46893c6e7ca1db67cb0f967f62c45d7ef289c77d
Ajit George <Ajit.George@MarkLogic.com>
2017-02-01 12:59:17 -0800

    Test changes for BT Issue 44107. Required rest-extension-user role and other privileges.

--- test-complete/src/test/java/com/marklogic/client/functionaltest/TestJSResourceExtensions.java
+++ test-complete/src/test/java/com/marklogic/client/functionaltest/TestJSResourceExtensions.java
@@ -16,29 +16,12 @@
 
 package com.marklogic.client.functionaltest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.html.FormSubmitEvent;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import org.json.JSONException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -54,8 +37,6 @@ import com.marklogic.client.admin.ExtensionMetadata.ScriptLanguage;
 import com.marklogic.client.admin.MethodType;
 import com.marklogic.client.admin.ResourceExtensionsManager;
 import com.marklogic.client.admin.ResourceExtensionsManager.MethodParameters;
-import com.marklogic.client.document.JSONDocumentManager;
-import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.extensions.ResourceManager;
 import com.marklogic.client.extensions.ResourceServices.ServiceResult;
 import com.marklogic.client.extensions.ResourceServices.ServiceResultIterator;
@@ -66,8 +47,6 @@ import com.marklogic.client.io.StringHandle;
 import com.marklogic.client.util.RequestParameters;
 
 public class TestJSResourceExtensions extends BasicJavaClientREST {
-	private static final int BATCH_SIZE=100;
-	private static final String DIRECTORY ="/bulkTransform/";
 	private static String dbName = "TestJSResourceExtensionDB";
 	private static String [] fNames = {"TestResourceExtensionDB-1"};
 	private static String restServerName = "REST-Java-Client-API-Server";
@@ -78,16 +57,10 @@ public class TestJSResourceExtensions extends BasicJavaClientREST {
 	static public class TestJSExtension extends ResourceManager {
 		static final public String NAME = "simpleJSResourceModule";
 		static final public ExtensionMetadata.ScriptLanguage scriptLanguage = ExtensionMetadata.JAVASCRIPT;
-		private JSONDocumentManager docMgr;
-
-
 		public TestJSExtension(DatabaseClient client) {
 			super();
 			// a Resource Manager must be initialized by a Database Client
 			client.init(NAME, this);
-
-			//  delegates some services to a document manager
-			docMgr = client.newJSONDocumentManager();
 		}
 
 		public String getJSON(String docUri) {
@@ -111,18 +84,17 @@ public class TestJSResourceExtensions extends BasicJavaClientREST {
 				result.getContent(readHandle);
 				responses.add(readHandle.get());
 			}
-
 			// release the iterator resources
 			resultItr.close();
-
 			return responses.get(0);
 		}
+		
 		public String postJSON(String docUri) {
 			RequestParameters params = new RequestParameters();
 			params.add("uri", docUri);
 			// specify the mime type for each expected document returned
 			String[] mimetypes = new String[] {"text/plain"};
-			StringHandle output = new StringHandle();
+			new StringHandle();
 			String input = "{\"array\" : [1,2,3]}";
 			// call the service
 			ServiceResultIterator resultItr= getServices().post(params, new StringHandle(input).withFormat(Format.JSON), mimetypes);
@@ -135,46 +107,41 @@ public class TestJSResourceExtensions extends BasicJavaClientREST {
 				result.getContent(readHandle);
 				responses.add(readHandle.get());
 			}
-
 			// release the iterator resources
 			resultItr.close();
 			return responses.get(0);
 		}
+		
 		public String putJSON(String docUri) {
 			RequestParameters params = new RequestParameters();
 			params.add("uri", docUri);
-			// specify the mime type for each expected document returned
-			String[] mimetypes = new String[] {"text/plain"};
-			StringHandle output = new StringHandle();
+			new StringHandle();
 			String input = "{\"argument1\":\"hello\", \"argument2\":\"Earth\", \"content\":\"This is a JSON document\", \"response\":[200, \"OK\"], \"outputTypes\":\"application/json\"}";
 			StringHandle readHandle = new StringHandle();
 			// call the service
 			getServices().put(params, new StringHandle(input).withFormat(Format.JSON), readHandle);
 			// iterate over the results
-
 			return readHandle.get();
 		}
+		
 		public String deleteJSON(String docUri) {
 			RequestParameters params = new RequestParameters();
 			params.add("uri", docUri);
-			// specify the mime type for each expected document returned
-			String[] mimetypes = new String[] {"text/plain"};
 			StringHandle output = new StringHandle();
 			// call the service
 			getServices().delete(params, output);
 			// iterate over the results
-
 			return output.get();
 		}
 	}
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println("In setup");
 		setupJavaRESTServer(dbName, fNames[0], restServerName,restPort,false);
-		createUserRolesWithPrevilages("test-eval","xdbc:eval", "xdbc:eval-in","xdmp:eval-in","any-uri","xdbc:invoke");
-	    createRESTUser("eval-user", "x", "test-eval","rest-admin","rest-writer","rest-reader");
-		//		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "debug");
-
+		createUserRolesWithPrevilages("test-eval","xdbc:eval", "xdbc:eval-in","xdmp:eval-in","any-uri", "xdmp-eval", "xdmp:value", "xdbc:invoke");
+	    createRESTUser("eval-user", "x", "test-eval","rest-admin","rest-writer","rest-reader", "rest-extension-user");
+		//System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "debug");
 	}
 
 	@AfterClass
@@ -202,20 +169,16 @@ public class TestJSResourceExtensions extends BasicJavaClientREST {
 		InputStreamHandle handle = new InputStreamHandle(myStream);
 		handle.set (myStream);
 		resourceMgr.writeServices("simpleJSResourceModule", handle, resextMetadata,getParams);
-
-
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		resourceMgr.deleteServices("simpleJSResourceModule");
 		client.release();
-
 	}
 
 	@Test
 	public void test1GetAllResourceServices() throws Exception {
-		
 		JacksonHandle jh = new JacksonHandle();
 		resourceMgr.listServices(jh);
 		String expectedList ="{\"resources\":{\"resource\":[{\"name\":\"simpleJSResourceModule\", \"source-format\":\"javascript\", \"description\":\"Testing resource extension for java script\", \"version\":\"1.0\", \"title\":\"BasicJSTest\", \"methods\":{\"method\":[{\"method-name\":\"get\", \"parameter\":[{\"parameter-name\":\"my-uri\", \"parameter-type\":\"xs:string?\"}]}, {\"method-name\":\"post\"}, {\"method-name\":\"put\"}, {\"method-name\":\"delete\"}]}, \"resource-source\":\"/v1/resources/simpleJSResourceModule\"}]}}";
@@ -229,42 +192,36 @@ public class TestJSResourceExtensions extends BasicJavaClientREST {
 		String expAftrPost ="{\"argument1\":\"helloJS.json\", \"argument2\":\"Earth\", \"document-count\":1, \"content\":\"This is a JSON document\", \"document-content\":{\"argument1\":\"hello\", \"argument2\":\"Earth\", \"content\":\"This is a JSON document\", \"array\":[1, 2, 3], \"response\":[200, \"OK\"], \"outputTypes\":\"application/json\"}, \"response\":[200, \"OK\"], \"outputTypes\":[\"application/json\"]}";
 		JSONAssert.assertEquals(expAftrPost, tjs.getJSON("helloJS.json"), false);
 		String expected ="{\"argument1\":\"helloJS.json\", \"argument2\":\"Earth\", \"document-count\":0, \"content\":\"This is a JSON document\", \"document-content\":null, \"response\":[200, \"OK\"], \"outputTypes\":[\"application/json\"]}";
-//		JSONAssert.assertEquals(expected, tjs.getJSON(), false);
 	
 		JSONAssert.assertEquals(expectedResponse, tjs.deleteJSON("helloJS.json"), false);
-//		System.out.println(tjs.getJSON());
 		JSONAssert.assertEquals(expected, tjs.getJSON("helloJS.json"), false);
-
 	}
+	
 	@Test
-	public void test2GetAllResourceServicesMultipleTimes() throws Exception {
-		
+	public void test2GetAllResourceServicesMultipleTimes() throws Exception {		
 		JacksonHandle jh = new JacksonHandle();
 		
 		TestJSExtension tjs= new TestJSExtension(client);
 		String expectedResponse="{\"response\":[200, \"OK\"]}";
 		//load multiple documents using extension
-		for(int i=0;i<150;i++){
-		JSONAssert.assertEquals(expectedResponse, tjs.putJSON("helloJS"+i+".json"), false);
-		JSONAssert.assertEquals(expectedResponse, tjs.postJSON("helloJS"+i+".json"), false);
+		for(int i=0;i<150;i++) {
+			JSONAssert.assertEquals(expectedResponse, tjs.putJSON("helloJS"+i+".json"), false);
+			JSONAssert.assertEquals(expectedResponse, tjs.postJSON("helloJS"+i+".json"), false);
 		}
 		
 		JacksonHandle jh2 = new JacksonHandle();
 		jh.set(jh2.getMapper().readTree(tjs.getJSON("helloJS0.json")));
-//		System.out.println(jh.get().toString());
 		assertEquals("Total documents loaded are",150,jh.get().get("document-count").intValue());
 
 		String expAftrPut ="{\"argument1\":\"hello\", \"argument2\":\"Earth\", \"content\":\"This is a JSON document\", \"array\":[1, 2, 3], \"response\":[200, \"OK\"], \"outputTypes\":\"application/json\"}";
 		String expected ="{\"argument1\":\"helloJS.json\", \"argument2\":\"Earth\", \"database-name\":\"TestJSResourceExtensionDB\", \"document-count\":0, \"content\":\"This is a JSON document\", \"document-content\":null, \"response\":[200, \"OK\"], \"outputTypes\":[\"application/json\"]}";
 //		verify by reading all the documents to see put and post services correctly inserted documents and delete them
-	    for(int j=0;j<150;j++){
-	    jh.set(jh2.getMapper().readTree(tjs.getJSON("helloJS"+j+".json")));	
-	    JSONAssert.assertEquals(expAftrPut,jh.get().get("document-content").findParent("array").toString(), false);
-	    JSONAssert.assertEquals(expectedResponse, tjs.deleteJSON("helloJS"+j+".json"), false);
-	    }
+		for(int j=0;j<150;j++) {
+			jh.set(jh2.getMapper().readTree(tjs.getJSON("helloJS"+j+".json")));	
+			JSONAssert.assertEquals(expAftrPut,jh.get().get("document-content").findParent("array").toString(), false);
+			JSONAssert.assertEquals(expectedResponse, tjs.deleteJSON("helloJS"+j+".json"), false);
+		}
 		System.out.println(tjs.getJSON("helloJS.json"));
 		JSONAssert.assertEquals(expected, tjs.getJSON("helloJS.json"), false);
-
 	}
-
 }
