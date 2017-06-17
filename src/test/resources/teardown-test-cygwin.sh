3a54b040010f7125e1a8fa001cf31eb734764a7c
Sam Mefford <sam.mefford@marklogic.com>
2017-06-16 18:33:15 -0600

    upgrade to new dependencies

--- src/test/resources/teardown-test-cygwin.sh
+++ src/test/resources/teardown-test-cygwin.sh
@@ -3,5 +3,5 @@
 DEFAULT_M2=$USERPROFILE/.m2/repository
 M2_REPO=${1:-$DEFAULT_M2}
 
-java -cp "target/test-classes;target/classes;$M2_REPO/org/apache/httpcomponents/httpclient/4.1.1/httpclient-4.1.1.jar;$M2_REPO/org/apache/httpcomponents/httpcore/4.1/httpcore-4.1.jar;$M2_REPO/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar;$M2_REPO/org/slf4j/slf4j-api/1.7.4/slf4j-api-1.7.4.jar" com.marklogic.client.test.util.TestServerBootstrapper teardown
+java -cp "target/test-classes;target/classes;$M2_REPO/org/apache/httpcomponents/httpclient/4.5.3/httpclient-4.5.3.jar;$M2_REPO/org/apache/httpcomponents/httpcore/4.4.6/httpcore-4.4.6.jar;$M2_REPO/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar;$M2_REPO/org/slf4j/slf4j-api/1.7.4/slf4j-api-1.7.4.jar" com.marklogic.client.test.util.TestServerBootstrapper teardown
 
