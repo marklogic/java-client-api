04c91c0744430a151dc34f3c0395e35c4479703e
Mads Hansen <mads.hansen@marklogic.com>
2018-01-14 15:23:30 -0500

    change EvalResultIterator interface to extend Closeable
    - add Closeable to EvalResultIterator
    - change EvalTest to utilize try-with-resources and remove the finally
    blocks explicitly closing

--- src/main/java/com/marklogic/client/eval/EvalResultIterator.java
+++ src/main/java/com/marklogic/client/eval/EvalResultIterator.java
@@ -15,12 +15,13 @@
  */
 package com.marklogic.client.eval;
 
+import java.io.Closeable;
 import java.util.Iterator;
 
 /** An Iterator to walk through all results returned from calls to
  * {@link ServerEvaluationCall#eval()}.
  */
-public interface EvalResultIterator extends Iterable<EvalResult>, Iterator<EvalResult> {
+public interface EvalResultIterator extends Iterable<EvalResult>, Iterator<EvalResult>, Closeable {
   @Override
   Iterator<EvalResult> iterator();
   @Override
