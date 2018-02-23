bbe11041e2a3bcf87e66f2aca028b95cf06f5e82
Vivek Muniyandi <vivek.siddharthan@gmail.com>
2018-02-22 18:24:45 -0800

    build.gradle

--- java-client-api/src/test/java/com/marklogic/client/test/DocumentMetadataHandleTest.java
+++ java-client-api/src/test/java/com/marklogic/client/test/DocumentMetadataHandleTest.java
@@ -198,7 +198,9 @@ public class DocumentMetadataHandleTest {
     Transaction transaction = Common.client.openTransaction();
     DocumentMetadataHandle metadataHandle = new DocumentMetadataHandle();
     documentManager.writeAs("all_well.xml", new FileHandle(
-          new File("test-complete/src/test/java/com/marklogic/client/functionaltest/data/all_well.xml")));
+          new File("../java-client-api-functionaltests/src/test/java/com" +
+            "/marklogic" +
+            "/client/functionaltest/data/all_well.xml")));
     InputStreamHandle handle = documentManager.read("all_well.xml", metadataHandle, new InputStreamHandle(), transaction);
     try {
       InputStream stream = handle.get();
