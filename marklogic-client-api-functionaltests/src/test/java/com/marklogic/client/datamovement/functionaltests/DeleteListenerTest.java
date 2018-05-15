c31eb6a45a6cccb742d040a8202c30e3d52fd545
georgeajit <Ajit.George@marklogic.com>
2018-05-15 15:30:23 -0700

    Git Issue #920 - Ignore the test until 920 issue is fixed

--- marklogic-client-api-functionaltests/src/test/java/com/marklogic/client/datamovement/functionaltests/DeleteListenerTest.java
+++ marklogic-client-api-functionaltests/src/test/java/com/marklogic/client/datamovement/functionaltests/DeleteListenerTest.java
@@ -169,7 +169,9 @@ public class DeleteListenerTest extends BasicJavaClientREST {
     clearDB(port);
   }
 
-  @Test
+  // Added Ignore until Git Issue 920 is fixed. This test causes entire test suite to hang.
+  // Zero Diff Task
+  @Ignore
   public void massDeleteSingleThread() throws Exception {
     Set<String> uriSet = new HashSet<>();
 
