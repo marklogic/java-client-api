c9bee77ad8069ce1ff96c3516229a70eb4047299
Vivek Siddharthan Muniyandi <vivek.siddharthan@gmail.com>
2018-05-30 14:16:17 -0700

    Missed the import statement

--- marklogic-client-api/src/test/java/com/marklogic/client/test/FailedRequestTest.java
+++ marklogic-client-api/src/test/java/com/marklogic/client/test/FailedRequestTest.java
@@ -18,6 +18,7 @@ package com.marklogic.client.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.Ignore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
