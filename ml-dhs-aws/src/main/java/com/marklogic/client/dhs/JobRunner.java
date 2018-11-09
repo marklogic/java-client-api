346913dac3c682ec3d90353cf86d5b80cd801421
asinha <anushree.sinha@marklogic.com>
2018-11-09 11:50:23 -0800

    Adding the after job document code.

--- ml-dhs-aws/src/main/java/com/marklogic/client/dhs/JobRunner.java
+++ ml-dhs-aws/src/main/java/com/marklogic/client/dhs/JobRunner.java
@@ -105,10 +105,12 @@ public class JobRunner {
          DocumentMetadataHandle beforeDocumentMetadata = new DocumentMetadataHandle();
          beforeDocumentMetadata.withCollections("/jobs/"+beforeDocId, "/beforeJob");
          
+         String ingestionStartTime = LocalDateTime.now().toString();
+         
          ObjectNode beforeDocRoot = objectMapper.createObjectNode();
          beforeDocRoot.put("job id", id);
          beforeDocRoot.put("jobMetadataValue",  jobMetadataValue);
-         beforeDocRoot.put("Date",  LocalDateTime.now().toString());
+         beforeDocRoot.put("ingestionStartTime",  ingestionStartTime);
          beforeDocRoot.put("headerValue",  headerValue);
          
          JacksonHandle jacksonHandle = new JacksonHandle(beforeDocRoot);
@@ -121,8 +123,28 @@ public class JobRunner {
          batcher.flushAndWait();
          moveMgr.stopJob(ticket);
          
+         String ingestionStopTime = LocalDateTime.now().toString();
 
-// TODO: write after job document with job metadata in /jobEnd and /jobs/ID collections or send after job payload to DHF endpoint
+// write after job document with job metadata in /jobEnd and /jobs/ID collections or send after job payload to DHF endpoint
+         String afterDocId = "/jobs/"+id+"/afterJob.json";
+         
+         DocumentMetadataHandle afterDocumentMetadata = new DocumentMetadataHandle();
+         afterDocumentMetadata.withCollections("/jobs/"+afterDocId, "/afterJob");
+         
+         ObjectNode afterDocRoot = objectMapper.createObjectNode();
+         afterDocRoot.put("job id", id);
+         afterDocRoot.put("jobMetadataValue",  jobMetadataValue);
+         afterDocRoot.put("ingestionStartTime",  ingestionStartTime);
+         afterDocRoot.put("ingestionStopTime",  ingestionStopTime);
+         
+         jacksonHandle = new JacksonHandle(afterDocRoot);
+         jsonMgr.write(afterDocId, afterDocumentMetadata, jacksonHandle);
+         
+         JobTicket afterTicket = moveMgr.startJob(batcher);
+
+         batcher.flushAndWait();
+         moveMgr.stopJob(afterTicket);
+         
       } finally {
          moveMgr.release();
       }
