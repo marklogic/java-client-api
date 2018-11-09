6b593f83848c65b323a0213d017673dd7df35852
asinha <anushree.sinha@marklogic.com>
2018-11-09 12:29:41 -0800

    Before and After job documents.

--- ml-dhs-aws/src/main/java/com/marklogic/client/dhs/JobRunner.java
+++ ml-dhs-aws/src/main/java/com/marklogic/client/dhs/JobRunner.java
@@ -1,15 +1,21 @@
 package com.marklogic.client.dhs;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.MarkLogicIOException;
 import com.marklogic.client.datamovement.DataMovementManager;
 import com.marklogic.client.datamovement.JobTicket;
 import com.marklogic.client.datamovement.WriteBatcher;
+import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.JacksonHandle;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.UUID;
 
 public class JobRunner {
@@ -37,10 +43,27 @@ public class JobRunner {
 
    public void run(DatabaseClient client, InputStream csvRecords, InputStream jobControl) throws IOException {
       DataMovementManager moveMgr = client.newDataMovementManager();
+      JSONDocumentManager jsonMgr = client.newJSONDocumentManager();
 
       try {
-         DocumentMetadataHandle documentMetadata = new DocumentMetadataHandle();
-         documentMetadata.withCollections("/jobs/"+jobId);
+          ObjectMapper objectMapper = new ObjectMapper();
+          
+          ObjectNode jobControlObj = (ObjectNode) objectMapper.readTree(jobControl);
+          ObjectNode jobDef        = (ObjectNode) jobControlObj.get("job");
+          ObjectNode recordDef     = (ObjectNode) jobControlObj.get("record");
+          
+          if(jobDef.isMissingNode()) {
+         	 throw new MarkLogicIOException("job Node cannot be empty.");
+          }
+          String id = jobDef.path("id").asText();
+          if(id==null || id.length()==0) {
+         	 throw new MarkLogicIOException("job id cannot be empty or Null");
+          }
+  		JsonNode metadataNode = jobDef.path("metadata");
+		String jobMetadataValue = metadataNode.asText();
+		DocumentMetadataHandle documentMetadata = new DocumentMetadataHandle();
+		documentMetadata.withCollections("/jobs/"+id);
+         
 // TODO: permissions
 
          WriteBatcher batcher = moveMgr.newWriteBatcher()
@@ -64,22 +87,64 @@ public class JobRunner {
                         throwable.printStackTrace(System.out);
                      });
 
-         ObjectMapper objectMapper = new ObjectMapper();
-         ObjectNode jobControlObj = (ObjectNode) objectMapper.readTree(jobControl);
-         ObjectNode jobDef        = (ObjectNode) jobControlObj.get("job");
-         ObjectNode recordDef     = (ObjectNode) jobControlObj.get("record");
 
          ObjectLoader loader = new ObjectLoader(batcher, recordDef, getJobDirectory(), documentMetadata);
-         CSVConverter converter = new CSVConverter();
+         CSVConverter converter = new CSVConverter();  
 
-// TODO: write before job document in /jobStart and /jobs/ID collections or send before job payload to DHF endpoint
-
+         Iterator<ObjectNode> itr = converter.convertObject(csvRecords);
+         if(!itr.hasNext()) {
+        	 throw new MarkLogicIOException("No header found.");
+         }
+         ObjectNode csvNode = itr.next();
+         
+         String headerValue = csvNode.fieldNames().toString();
+         
+   // write before job document in /jobStart and /jobs/ID collections or send before job payload to DHF endpoint        
+         String beforeDocId = "/jobs/"+id+"/beforeJob.json";
+         
+         DocumentMetadataHandle beforeDocumentMetadata = new DocumentMetadataHandle();
+         beforeDocumentMetadata.withCollections("/jobs/"+beforeDocId, "/beforeJob");
+         
+         String ingestionStartTime = LocalDateTime.now().toString();
+         
+         ObjectNode beforeDocRoot = objectMapper.createObjectNode();
+         beforeDocRoot.put("job id", id);
+         beforeDocRoot.put("jobMetadataValue",  jobMetadataValue);
+         beforeDocRoot.put("ingestionStartTime",  ingestionStartTime);
+         beforeDocRoot.put("headerValue",  headerValue);
+         
+         JacksonHandle jacksonHandle = new JacksonHandle(beforeDocRoot);
+         jsonMgr.write(beforeDocId, beforeDocumentMetadata, jacksonHandle);
+         
          JobTicket ticket = moveMgr.startJob(batcher);
-         loader.loadRecords(converter.convertObject(csvRecords));
+         loader.loadRecord(csvNode);
+         
+         loader.loadRecords(itr);
          batcher.flushAndWait();
          moveMgr.stopJob(ticket);
+         
+         String ingestionStopTime = LocalDateTime.now().toString();
 
-// TODO: write after job document with job metadata in /jobEnd and /jobs/ID collections or send after job payload to DHF endpoint
+// write after job document with job metadata in /jobEnd and /jobs/ID collections or send after job payload to DHF endpoint
+         String afterDocId = "/jobs/"+id+"/afterJob.json";
+         
+         DocumentMetadataHandle afterDocumentMetadata = new DocumentMetadataHandle();
+         afterDocumentMetadata.withCollections("/jobs/"+afterDocId, "/afterJob");
+         
+         ObjectNode afterDocRoot = objectMapper.createObjectNode();
+         afterDocRoot.put("job id", id);
+         afterDocRoot.put("jobMetadataValue",  jobMetadataValue);
+         afterDocRoot.put("ingestionStartTime",  ingestionStartTime);
+         afterDocRoot.put("ingestionStopTime",  ingestionStopTime);
+         
+         jacksonHandle = new JacksonHandle(afterDocRoot);
+         jsonMgr.write(afterDocId, afterDocumentMetadata, jacksonHandle);
+         
+         JobTicket afterTicket = moveMgr.startJob(batcher);
+
+         batcher.flushAndWait();
+         moveMgr.stopJob(afterTicket);
+         
       } finally {
          moveMgr.release();
       }
