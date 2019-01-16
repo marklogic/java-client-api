9dbb10ee09e0ef26a312d42953be50c1ae4f157c
asinha <anushree.sinha@marklogic.com>
2019-01-16 10:46:14 -0800

    Adding the copyright and javadoc.

--- marklogic-client-api/src/main/java/com/marklogic/client/impl/HTTPSamlAuthInterceptor.java
+++ marklogic-client-api/src/main/java/com/marklogic/client/impl/HTTPSamlAuthInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.marklogic.client.impl;
 
 import java.io.IOException;
@@ -9,12 +25,6 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
-/**
- * An HTTP Request interceptor that modifies the request headers to enable
- * 'SAML' authentication. It compares the current instant with the expiry time of the existing token and 
- * resets the token if the current time is greater than or equal to half of the expiry time.
- *
- */
 public class HTTPSamlAuthInterceptor implements Interceptor{
 
 	  private String authorizationTokenValue;
