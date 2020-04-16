8b0305f1171ef6624d5481da94015fd28766578a
anu3990 <anushreegithub@gmail.com>
2020-04-15 17:03:15 -0700

    Adding BulkExportWithDataServiceTest.

---
+++ marklogic-client-api/src/test/java/com/marklogic/client/test/example/cookbook/BulkExportWithDataServiceTest.java
@@ -0,0 +1,11 @@
+package com.marklogic.client.test.example.cookbook;
+
+import com.marklogic.client.example.cookbook.datamovement.BulkExportWithDataService;
+import org.junit.Test;
+
+public class BulkExportWithDataServiceTest {
+    @Test
+    public void testMain() throws Exception {
+        BulkExportWithDataService.main(new String[0]);
+    }
+}
