fd5c8f34e88a0b1557cf6efc3f32b4270b5f4a48
lling <lling@marklogic.com>
2020-05-18 17:00:08 -0700

    Add class and unit test for #1223

--- marklogic-client-api/src/test/java/com/marklogic/client/test/datamovement/LineSplitterTest.java
+++ marklogic-client-api/src/test/java/com/marklogic/client/test/datamovement/LineSplitterTest.java
@@ -1,6 +1,8 @@
-package com.marklogic.client.test;
+package com.marklogic.client.test.datamovement;
 
 import com.marklogic.client.datamovement.LineSplitter;
+import com.marklogic.client.datamovement.Splitter;
+import com.marklogic.client.document.DocumentWriteOperation;
 import com.marklogic.client.io.Format;
 import com.marklogic.client.io.StringHandle;
 import org.junit.*;
@@ -11,15 +13,16 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 
 import static org.junit.Assert.*;
 
 public class LineSplitterTest {
-    static final private String jsonlFile = "src/test/resources/data" + File.separator + "line-delimited.jsonl";
-    static final private String jsonlGzipFile = "src/test/resources/data" + File.separator + "line-delimited.jsonl.gz";
-    static final private String xmlFile = "src/test/resources/data" + File.separator + "line-delimited.txt";
+    static final private String jsonlFile = "src/test/resources/data" + File.separator + "pathSplitter/line-delimited.jsonl";
+    static final private String jsonlGzipFile = "src/test/resources/data" + File.separator + "pathSplitter/line-delimited.jsonl.gz";
+    static final private String xmlFile = "src/test/resources/data" + File.separator + "pathSplitter/line-delimited.txt";
 
     @Test
     public void testSplitter() throws Exception {
@@ -74,6 +77,34 @@ public class LineSplitterTest {
             assertNotNull(result[i].get());
             assertEquals(result[i].get(), originalResult[i]);
         }
+    }
+
+    @Test
+    public void testSplitterDocumentWriteOperation() throws Exception {
+        LineSplitter splitter = new LineSplitter();
+        splitter.setFormat(Format.XML);
+        Stream<DocumentWriteOperation> contentStream = splitter.splitWriteOperations(new FileInputStream(xmlFile), "line-delimited.txt");
+        Splitter.UriMaker uriMaker = splitter.getUriMaker();
+        uriMaker.setInputAfter("/test/");
+        assertNotNull(contentStream);
+
+        String[] originalResult = Files.lines(Paths.get(xmlFile))
+                .toArray(size -> new String[size]);
+
+        Iterator<DocumentWriteOperation> itr = contentStream.iterator();
+        int i = 0;
+        while (itr.hasNext()) {
+            DocumentWriteOperation docOp = itr.next();
+            String uri = docOp.getUri();
+            assertNotNull(docOp.getUri());
+
+            assertNotNull(docOp.getContent());
+            String docOpContent = docOp.getContent().toString();
+            assertEquals(docOpContent, originalResult[i]);
+            i++;
+        }
+
+        assertEquals(4, splitter.getCount());
     }
 
 }
