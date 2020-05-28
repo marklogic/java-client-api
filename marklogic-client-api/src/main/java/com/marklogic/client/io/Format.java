ae70e2852b6dc2fc6a1d7d47dbc56091ec110520
lling <lling@marklogic.com>
2020-05-28 10:33:41 -0700

    some modifications for #1223

--- marklogic-client-api/src/main/java/com/marklogic/client/io/Format.java
+++ marklogic-client-api/src/main/java/com/marklogic/client/io/Format.java
@@ -73,6 +73,23 @@ public enum Format {
     }
   }
 
+  public String getDefaultExtension() {
+    switch(this) {
+      case UNKNOWN:
+        return null;
+      case BINARY:
+        return "bin";
+      case JSON:
+        return "json";
+      case TEXT:
+        return "txt";
+      case XML:
+        return "xml";
+      default:
+        throw new MarkLogicInternalException("Unknown format "+this.toString());
+    }
+  }
+
   /**
    * Returns the format implied by the mime type
    * @param mimeType  the mime type
