dd4f8794ae1951c673e0cc62c530cbae099a8545
ehennum <ehennum@marklogic.com>
2020-06-02 15:48:50 -0700

    RowBatcher CSV and XML unit tests

--- marklogic-client-api/src/test/java/com/marklogic/client/test/datamovement/RowBatcherTest.java
+++ marklogic-client-api/src/test/java/com/marklogic/client/test/datamovement/RowBatcherTest.java
@@ -25,9 +25,7 @@ import com.marklogic.client.document.DocumentWriteSet;
 import com.marklogic.client.document.JSONDocumentManager;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.expression.PlanBuilder;
-import com.marklogic.client.io.DocumentMetadataHandle;
-import com.marklogic.client.io.JacksonHandle;
-import com.marklogic.client.io.StringHandle;
+import com.marklogic.client.io.*;
 import com.marklogic.client.query.DeleteQueryDefinition;
 import com.marklogic.client.query.QueryManager;
 import com.marklogic.client.row.RowManager;
@@ -37,19 +35,27 @@ import com.marklogic.client.type.PlanSystemColumn;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 
 public class RowBatcherTest {
     private final static String TEST_DIR = "/test/rowbatch/unit/";
     private final static String TEST_COLLECTION = TEST_DIR+"codes";
+    private final static String TABLE_NS_URI = "http://marklogic.com/table";
 
     private final static int TEST_DOC_COUNT = 120;
 
@@ -170,44 +176,62 @@ public class RowBatcherTest {
     }
 
     @Test
-    public void testRows1Thread() throws Exception {
-        runRowsTest(jsonBatcher(1));
+    public void testJsonRows1Thread() throws Exception {
+        runJsonRowsTest(jsonBatcher(1));
     }
     @Test
-    public void testRows3Threads() throws Exception {
-        runRowsTest(jsonBatcher(3));
+    public void testJsonRows3Threads() throws Exception {
+        runJsonRowsTest(jsonBatcher(3));
     }
     @Test
-    public void testRowsForest2Threads() throws Exception {
-        runRowsTest(jsonBatcher(moveMgr.readForestConfig().listForests().length * 2));
+    public void testJsonRowsForest2Threads() throws Exception {
+        runJsonRowsTest(jsonBatcher(moveMgr.readForestConfig().listForests().length * 2));
     }
     @Test
-    public void testDocs1Thread() throws Exception {
+    public void testJsonDocs1Thread() throws Exception {
         runDocsTest(jsonBatcher(1));
     }
     @Test
-    public void testDocs3Threads() throws Exception {
+    public void testJsonDocs3Threads() throws Exception {
         runDocsTest(jsonBatcher(3));
     }
     @Test
-    public void testDocsForest2Threads() throws Exception {
+    public void testJsonDocsForest2Threads() throws Exception {
         runDocsTest(jsonBatcher(moveMgr.readForestConfig().listForests().length * 2));
     }
     @Test
-    public void testRows3ThreadsPointInTime() throws Exception {
-        runRowsTest(jsonBatcher(3), true);
+    public void testJsonRows3ThreadsPointInTime() throws Exception {
+        runJsonRowsTest(jsonBatcher(3), true);
     }
+    @Test
+    public void testXmlRows3Threads() throws Exception {
+        runXmlRowsTest(xmlBatcher(3));
+    }
+    @Test
+    public void testCsvRows3Threads() throws Exception {
+        runCsvRowsTest(csvBatcher(3));
+    }
+    /* TODO: style tests
+     */
     private RowBatcher<JsonNode> jsonBatcher(int threads) {
         return moveMgr.newRowBatcher(new JacksonHandle())
                         .withBatchSize(30)
                         .withThreadCount(threads);
     }
-    /* TODO: formats, styles
-     */
-    private void runRowsTest(RowBatcher<JsonNode> rowBatcher) throws Exception {
-        runRowsTest(rowBatcher, false);
+    private RowBatcher<Document> xmlBatcher(int threads) {
+        return moveMgr.newRowBatcher(new DOMHandle())
+                .withBatchSize(30)
+                .withThreadCount(threads);
     }
-    private void runRowsTest(RowBatcher<JsonNode> rowBatcher, boolean consistentSnapshot) throws Exception {
+    private RowBatcher<String> csvBatcher(int threads) {
+        return moveMgr.newRowBatcher(new StringHandle().withFormat(Format.TEXT).withMimetype("text/csv"))
+                .withBatchSize(30)
+                .withThreadCount(threads);
+    }
+    private void runJsonRowsTest(RowBatcher<JsonNode> rowBatcher) throws Exception {
+        runJsonRowsTest(rowBatcher, false);
+    }
+    private void runJsonRowsTest(RowBatcher<JsonNode> rowBatcher, boolean consistentSnapshot) throws Exception {
         if (consistentSnapshot) {
             rowBatcher.withConsistentSnapshot();
         }
@@ -351,5 +375,198 @@ public class RowBatcherTest {
         assertEquals("mismatch on row estimate", expectedDoc.size(), rowBatcher.getRowEstimate());
         assertEquals("mismatch on actual rows size", expectedDoc.size(), actual.size());
         assertEquals("mismatch on actual docs", expectedDoc, actual);
+    }
+    private void runXmlRowsTest(RowBatcher<Document> rowBatcher) throws Exception {
+        RowManager rowMgr = rowBatcher.getRowManager();
+        rowMgr.setDatatypeStyle(RowManager.RowSetPart.HEADER);
+
+        PlanBuilder planBuilder = rowMgr.newPlanBuilder();
+        PlanBuilder.ModifyPlan plan =
+                planBuilder.fromView("rowBatcherUnitTest", "code", "");
+
+        Set<String> actual = new ConcurrentSkipListSet<>();
+        AtomicBoolean failed = new AtomicBoolean(false);
+        rowBatcher.withBatchView(plan)
+                .onSuccess(event -> {
+                    /* System.out.println("succeeded batch="+event.getJobBatchNumber()+
+                            " from "+event.getLowerBound()+" through "+event.getUpperBound()+
+                            ((event.getJobBatchNumber() == 1) ? "\n"+Common.testDocumentToString(event.getRowsDoc()) : "")); */
+                    try {
+                        NodeList rows = event.getRowsDoc().getDocumentElement().getElementsByTagNameNS(TABLE_NS_URI, "rows");
+                        if (rows == null || rows.getLength() < 1) {
+                            System.out.println("no rows for batch="+event.getJobBatchNumber()+
+                                    " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                            failed.set(true);
+                            return;
+                        }
+                        // System.out.println("starting batch="+event.getJobBatchNumber()+" for "+rows.size()+" rows");
+                        for (int i=0; i < rows.getLength(); i++) {
+                            NodeList cells = ((Element) rows.item(i)).getElementsByTagNameNS(TABLE_NS_URI, "cell");
+                            if (cells == null || cells.getLength() < 1) {
+                                System.out.println("no cells for row "+i+
+                                        " in batch="+event.getJobBatchNumber()+
+                                        " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                                failed.set(true);
+                                return;
+                            }
+                            boolean[] fields = new boolean[4];
+                            Arrays.fill(fields, false);
+                            for (int j=0; j < cells.getLength(); j++) {
+                                Element cell = (Element) cells.item(j);
+                                String cellName = cell.getAttribute("name");
+                                switch (cellName) {
+                                case "full":
+                                    String cellText = cell.getTextContent();
+                                    if (actual.contains(cellText)) {
+                                        System.out.println("already found full="+cellText);
+                                        failed.set(true);
+                                        return;
+                                    } else {
+                                        actual.add(cellText);
+                                    }
+                                    break;
+                                case "field1":
+                                case "field2":
+                                case "field3":
+                                case "field4":
+                                    int k = Integer.parseInt(cellName.substring(cellName.length() - 1)) - 1;
+                                    if (k < fields.length) {
+                                        fields[k] = true;
+                                    } else {
+                                        System.out.println("field mismatch "+cellName+" in row "+i+
+                                                " of batch="+event.getJobBatchNumber()+
+                                                " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                                        failed.set(true);
+                                        return;
+                                    }
+                                    break;
+                                // TODO: remove rowid
+                                case "rowid":
+                                    break;
+                                default:
+                                    System.out.println("unknown cell "+cellName+" in row "+i+" of batch="+event.getJobBatchNumber()+
+                                            " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                                    failed.set(true);
+                                    return;
+                                }
+                            }
+                            for (int j=0; j < fields.length; j++) {
+                                if (!(fields[j])) {
+                                    System.out.println("failed to find field"+j+" cell in row "+i+
+                                            " of batch="+event.getJobBatchNumber()+
+                                            " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                                    failed.set(true);
+                                    return;
+                                }
+                            }
+                        }
+                        // System.out.println("finish batch="+event.getJobBatchNumber());
+                    } catch (Throwable e) {
+                        failed.set(true);
+                        System.out.println(e.getMessage());
+                        e.printStackTrace(System.out);
+                    }
+                })
+                .onFailure((event, throwable) -> {
+                    failed.set(true);
+                    System.out.println("failed batch="+event.getJobBatchNumber()+
+                            " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                });
+
+        moveMgr.startJob(rowBatcher);
+        rowBatcher.awaitCompletion();
+        // System.out.println("stopped="+rowBatcher.isStopped());
+
+        assertEquals("test execution failed", false, failed.get());
+        assertEquals("mismatch on row estimate", expectedFull.size(), rowBatcher.getRowEstimate());
+        assertEquals("mismatch on actual rows size", expectedFull.size(), actual.size());
+        assertEquals("mismatch on actual rows", expectedFull, actual);
+    }
+    private void runCsvRowsTest(RowBatcher<String> rowBatcher) throws Exception {
+        RowManager rowMgr = rowBatcher.getRowManager();
+        rowMgr.setDatatypeStyle(RowManager.RowSetPart.HEADER);
+
+        PlanBuilder planBuilder = rowMgr.newPlanBuilder();
+        PlanBuilder.ModifyPlan plan =
+                planBuilder.fromView("rowBatcherUnitTest", "code", "");
+
+        Pattern commaSplitter = Pattern.compile(",");
+
+        Set<String> actual = new ConcurrentSkipListSet<>();
+        AtomicBoolean failed = new AtomicBoolean(false);
+        rowBatcher.withBatchView(plan)
+                .onSuccess(event -> {
+                    /* System.out.println("succeeded batch="+event.getJobBatchNumber()+
+                            " from "+event.getLowerBound()+" through "+event.getUpperBound()+
+                            ((event.getJobBatchNumber() == 1) ? "\n"+event.getRowsDoc() : "")); */
+                    try {
+                        BufferedReader reader = new BufferedReader(new StringReader(event.getRowsDoc()));
+                        String[] columns = commaSplitter.split(reader.readLine());
+                        // TODO: CHANGE TO 5 AFTER REMOVING rowid COLUMN
+                        if (columns.length != 6) {
+                            System.out.println("unexpected number of columns "+columns.length+
+                                    " for batch="+event.getJobBatchNumber()+
+                                    " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                        }
+                        AtomicInteger fullColNum = new AtomicInteger(-1);
+                        for (int i=0; i < columns.length; i++) {
+                            String colName = columns[i];
+                            switch (colName) {
+                            case "full":
+                                fullColNum.set(i);
+                                break;
+                            case "field1":
+                            case "field2":
+                            case "field3":
+                            case "field4":
+                            // TODO: remove rowid
+                            case "rowid":
+                                break;
+                            default:
+                                System.out.println("unknown column "+colName+" of batch="+event.getJobBatchNumber()+
+                                        " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                                failed.set(true);
+                                return;
+                            }
+                        }
+                        // System.out.println("starting batch="+event.getJobBatchNumber());
+                        reader.lines().forEach(line -> {
+                            // System.out.println(line);
+                            String[] cells = commaSplitter.split(line);
+                            if (cells.length < columns.length) {
+                                System.out.println("unexpected number of cells "+cells.length+
+                                        " in row for for batch="+event.getJobBatchNumber()+
+                                        " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                            }
+                            String full = cells[fullColNum.get()];
+                            if (actual.contains(full)) {
+                                System.out.println("already found full="+full);
+                                failed.set(true);
+                                return;
+                            } else {
+                                actual.add(full);
+                            }
+                        });
+                        // System.out.println("finish batch="+event.getJobBatchNumber());
+                    } catch (Throwable e) {
+                        failed.set(true);
+                        System.out.println(e.getMessage());
+                        e.printStackTrace(System.out);
+                    }
+                })
+                .onFailure((event, throwable) -> {
+                    failed.set(true);
+                    System.out.println("failed batch="+event.getJobBatchNumber()+
+                            " from "+event.getLowerBound()+" through "+event.getUpperBound());
+                });
+
+        moveMgr.startJob(rowBatcher);
+        rowBatcher.awaitCompletion();
+        // System.out.println("stopped="+rowBatcher.isStopped());
+
+        assertEquals("test execution failed", false, failed.get());
+        assertEquals("mismatch on row estimate", expectedFull.size(), rowBatcher.getRowEstimate());
+        assertEquals("mismatch on actual rows size", expectedFull.size(), actual.size());
+        assertEquals("mismatch on actual rows", expectedFull, actual);
     }
 }
