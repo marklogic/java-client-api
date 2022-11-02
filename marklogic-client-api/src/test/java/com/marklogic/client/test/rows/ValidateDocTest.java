4ba664148a22a8f0d82319503ed61f8d2a638cd8
anu3990 <anushreegithub@gmail.com>
2022-11-02 09:16:23 -0700

    JAVA-242 : Tests for "validateDoc" pipeline operation

--- marklogic-client-api/src/test/java/com/marklogic/client/test/rows/ValidateDocTest.java
+++ marklogic-client-api/src/test/java/com/marklogic/client/test/rows/ValidateDocTest.java
@@ -15,28 +15,23 @@
  */
 package com.marklogic.client.test.rows;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.marklogic.client.datamovement.DataMovementManager;
 import com.marklogic.client.datamovement.WriteBatcher;
 import com.marklogic.client.document.DocumentManager;
+import com.marklogic.client.document.DocumentWriteSet;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.expression.PlanBuilder;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.Format;
-import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.client.io.StringHandle;
-import com.marklogic.client.io.marker.AbstractWriteHandle;
 import com.marklogic.client.row.RowRecord;
 import com.marklogic.client.test.Common;
-import com.marklogic.client.type.PlanParamExpr;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.util.StringUtils;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static com.marklogic.client.io.Format.JSON;
 import static com.marklogic.client.io.Format.XML;
 import static org.junit.Assert.*;
 
@@ -49,9 +44,12 @@ public class ValidateDocTest extends AbstractOpticUpdateTest {
     public void moreSetup(){
         dataMovementManager = Common.client.newDataMovementManager();
     }
-// TODO : test with rest-writer user when https://bugtrack.marklogic.com/57978 is fixed
+
     @Test
-    public void xmlSchema() {
+    public void testXmlSchemaWithLaxMode() {
+        if (!Common.markLogicIsVersion11OrHigher()) {
+            return;
+        }
         WriteBatcher writeBatcher = dataMovementManager.newWriteBatcher();
         DocumentMetadataHandle meta = newDefaultMetadata();
         dataMovementManager.startJob(writeBatcher);
@@ -75,36 +73,108 @@ public class ValidateDocTest extends AbstractOpticUpdateTest {
 
         List<RowRecord> rows = resultRows(plan);
 
-        //TODO : uncomment lines 84 and 89 after https://bugtrack.marklogic.com/57987 is fixed
         List<String> persistedUris = rows.stream().map(row -> {
             String uri = row.getString("uri");
-            if (StringUtils.isEmpty(uri)) {
-                //fail("URI returned by resultRows is null: " + row);
-            }
             return uri;
         }).collect(Collectors.toList());
         assertEquals(uriCountToWrite, persistedUris.size());
-        //expectedUris.forEach(uri -> assertTrue("Did not find URI: " + uri, persistedUris.contains(uri)));
+    }
+
+    @Test
+    public void testXmlSchemaWithStrictMode() {
+        if (!Common.markLogicIsVersion11OrHigher()) {
+            return;
+        }
+        String[][] triples = new String[][]{
+                new String[]{"http://example.org/rowgraph/s1", "http://example.org/rowgraph/p1", "http://example.org/rowgraph/o1"},
+                new String[]{"http://example.org/rowgraph/s1", "http://example.org/rowgraph/p2", "http://example.org/rowgraph/o2"},
+                new String[]{"http://example.org/rowgraph/s2", "http://example.org/rowgraph/p1", "http://example.org/rowgraph/o3"}
+        };
+        XMLDocumentManager docMgr = Common.client.newXMLDocumentManager();
+
+        String triplesXML =
+                "<sem:triples xmlns:sem=\"http://marklogic.com/semantics\">\n"+
+                        String.join("\n", (String[]) Arrays
+                                .stream(triples)
+                                .map(triple ->
+                                        "<sem:triple>"+
+                                                "<sem:subject>"+triple[0]+"</sem:subject>"+
+                                                "<sem:predicate>"+triple[1]+"</sem:predicate>"+
+                                                "<sem:object>"+triple[2]+"</sem:object>"+
+                                                "</sem:triple>"
+                                )
+                                .toArray(size -> new String[size]))+
+                        "</sem:triples>";
+        DocumentWriteSet writeSet = Common.client.newDocumentManager().newWriteSet();
+        DocumentMetadataHandle metadata = newDefaultMetadata();
+        for(int i=0; i<4; i++){
+            writeSet.add("/acme/"+i+".xml",metadata, new StringHandle(triplesXML).withFormat(Format.XML));
+        }
+        docMgr.write(writeSet);
+
+        PlanBuilder.Plan plan = op
+                .fromDocUris(op.cts.directoryQuery("/acme/"))
+                .joinDoc(op.col("doc"), op.col("uri"))
+                .validateDoc(op.col("doc"),
+                        op.schemaDefinition("xmlSchema").withMode("strict")
+                );
+        Iterator<RowRecord> rows = rowManager.resultRows(plan).iterator();
+        Set<String> uris = new HashSet<>();
+        while (rows.hasNext()){
+            uris.add(rows.next().getString("uri"));
+        }
+         assertTrue(uris.size() == 4);
+    }
+
+    @Test
+    public void testWriteWithXmlSchemaWithStrictMode() {
+        if (!Common.markLogicIsVersion11OrHigher()) {
+            return;
+        }
+        String[][] triples = new String[][]{
+                new String[]{"http://example.org/rowgraph/s1", "http://example.org/rowgraph/p1", "http://example.org/rowgraph/o1"},
+                new String[]{"http://example.org/rowgraph/s1", "http://example.org/rowgraph/p2", "http://example.org/rowgraph/o2"},
+                new String[]{"http://example.org/rowgraph/s2", "http://example.org/rowgraph/p1", "http://example.org/rowgraph/o3"}
+        };
+
+        String triplesXML =
+                "<sem:triples xmlns:sem=\"http://marklogic.com/semantics\">\n"+
+                        String.join("\n", (String[]) Arrays
+                                .stream(triples)
+                                .map(triple ->
+                                        "<sem:triple>"+
+                                                "<sem:subject>"+triple[0]+"</sem:subject>"+
+                                                "<sem:predicate>"+triple[1]+"</sem:predicate>"+
+                                                "<sem:object>"+triple[2]+"</sem:object>"+
+                                                "</sem:triple>"
+                                )
+                                .toArray(size -> new String[size]))+
+                        "</sem:triples>";
+        DocumentWriteSet writeSet = Common.client.newDocumentManager().newWriteSet();
+        DocumentMetadataHandle metadata = newDefaultMetadata();
+        for(int i=0; i<4; i++){
+            writeSet.add("/acme/"+i+".xml", metadata, new StringHandle(triplesXML).withFormat(Format.XML));
+        }
+
+        PlanBuilder.Plan plan = op.fromParam("myDocs", "", op.docColTypes())
+                .validateDoc(op.col("doc"),
+                        op.schemaDefinition("xmlSchema").withMode("strict"))
+                .write();
+        plan = plan.bindParam("myDocs", writeSet);
+
+        List<RowRecord> rows = resultRows(plan);
+        assertEquals(4, rows.size());
     }
 
     @Test
     public void testUsingFromParamAndSchematron() {
+        if (!Common.markLogicIsVersion11OrHigher()) {
+            return;
+        }
+        DocumentWriteSet writeSet = Common.client.newDocumentManager().newWriteSet();
+        DocumentMetadataHandle metadata = newDefaultMetadata();
 
-        PlanBuilder.Plan plan = op.fromParam("bindingParam", "", op.colTypes(
-                op.colType("rowId", "integer"),
-                op.colType("doc")
-        ))
-        .validateDoc(op.col("doc"),
-                op.schemaDefinition("schematron").withSchemaUri("/validateDoc/schematron.sch")
-        );
-
-        final PlanParamExpr param = op.param("bindingParam");
-        ArrayNode array = mapper.createArrayNode();
-        array.addObject().put("rowId", 1).put("doc", "doc1.xml");
-        array.addObject().put("rowId", 2).put("doc", "doc2.xml");
-        array.addObject().put("rowId", 3).put("doc", "doc3.xml");
-        Map<String, AbstractWriteHandle> attachments = new HashMap<>();
-        attachments.put("doc1.xml", new StringHandle("<user id=\"001\">\n" +
+        writeSet.add("/acme/doc1.xml", metadata, new StringHandle("<user id=\"001\">\n" +
                 "  <name>Alan</name>\n" +
                 "  <gender>Male</gender>\n" +
                 "  <age>14</age>\n" +
@@ -114,7 +184,7 @@ public class ValidateDocTest extends AbstractOpticUpdateTest {
                 "  </score>  \n" +
                 "  <result>fail</result> \n" +
                 "</user>").withFormat(Format.XML));
-        attachments.put("doc2.xml", new StringHandle("<user id=\"002\">\n" +
+        writeSet.add("/acme/doc2.xml", metadata, new StringHandle("<user id=\"002\">\n" +
                 "  <name>John</name>\n" +
                 "  <gender>Male</gender>\n" +
                 "  <age>15</age>\n" +
@@ -124,7 +194,8 @@ public class ValidateDocTest extends AbstractOpticUpdateTest {
                 "  </score>  \n" +
                 "  <result>fail</result> \n" +
                 "</user>").withFormat(Format.XML));
-        attachments.put("doc3.xml", new StringHandle("<user id=\"003\">\n" +
+
+        writeSet.add("/acme/doc3.xml", metadata, new StringHandle("<user id=\"003\">\n" +
                 "  <name>George</name>\n" +
                 "  <gender>Male</gender>\n" +
                 "  <age>15</age>\n" +
@@ -134,51 +205,97 @@ public class ValidateDocTest extends AbstractOpticUpdateTest {
                 "  </score>  \n" +
                 "  <result>pass</result> \n" +
                 "</user>").withFormat(Format.XML));
-        plan = plan.bindParam(param, new JacksonHandle(array), Collections.singletonMap("doc", attachments));
+        PlanBuilder.Plan plan = op.fromParam("myDocs", "", op.docColTypes())
+                .validateDoc(op.col("doc"),
+                        op.schemaDefinition("schematron").withSchemaUri("/validateDoc/schematron.sch")
+                ).write();
+        plan = plan.bindParam("myDocs", writeSet);
 
-        Iterator<RowRecord> rows = rowManager.resultRows(plan).iterator();
-        while (rows.hasNext()){
-            RowRecord rowRecord = rows.next();
-            if(rowRecord.getString("rowId")!=null)
-                expectedUris.add(rowRecord.getString("rowId"));
-        }
-        // TODO: uncomment the below after https://bugtrack.marklogic.com/58055 is fixed
-        // assertTrue(expectedUris.size() == 1);
-        // assertTrue(expectedUris.contains("3"));
+        List<RowRecord> rows = resultRows(plan);
+        assertEquals(1, rows.size());
+        DocumentManager docMgr = Common.client.newDocumentManager();
+        assertTrue("Document /acme/doc1.xml should not exist",docMgr.exists("/acme/doc1.xml") == null);
+        assertTrue("Document /acme/doc2.xml should not exist",docMgr.exists("/acme/doc2.xml") == null);
+        assertTrue("Document /acme/doc3.xml does not exist",docMgr.exists("/acme/doc3.xml") != null);
     }
 
     @Test
-    public void jsonSchema() {
+    public void testWithJsonSchema() {
+        if (!Common.markLogicIsVersion11OrHigher()) {
+            return;
+        }
         PlanBuilder.ModifyPlan plan = op
                 .fromDocDescriptors(
                         op.docDescriptor(newWriteOp("/acme/doc1.json", mapper.createObjectNode().put("count", 1).put("total",2))),
                         op.docDescriptor(newWriteOp("/acme/doc2.json", mapper.createObjectNode().put("count", 2).put("total",3)))
                 )
                 .validateDoc(op.col("doc"),
-                        op.schemaDefinition("jsonSchema").withSchemaUri("/validateDoc/jsonSchema.json")
-                );
+                        op.schemaDefinition("jsonSchema").withSchemaUri("/validateDoc/jsonSchema.json"));
 
-        // TODO : uncomment below block after https://bugtrack.marklogic.com/58025 is fixed
-        /*verifyExportedPlanReturnsSameRowCount(plan);
+        verifyExportedPlanReturnsSameRowCount(plan);
         Iterator<RowRecord> rows = rowManager.resultRows(plan).iterator();
         while (rows.hasNext()){
             RowRecord str = rows.next();
             String uri = str.getString("uri");
-            if(uri!=null){
-                System.out.println(str.getString("doc"));
                 expectedUris.add(uri);
-            }
         }
-        */
-
-        // TODO: uncomment the below after https://bugtrack.marklogic.com/57987 is fixed
-        /*
+        assertTrue(expectedUris.size()==2);
         rowManager.execute(plan.write());
         DocumentManager docMgr = Common.client.newDocumentManager();
-        assertTrue(docMgr.exists("/acme/doc1.json")!=null);
-        assertTrue(docMgr.exists("/acme/doc2.json")!=null);
-        assertTrue(expectedUris.size()==2);
-        assertTrue(expectedUris.contains("/acme/doc1.json"));
-        assertTrue(expectedUris.contains("/acme/doc2.json"));*/
+        assertTrue("Document /acme/doc1.json does not exist",docMgr.exists("/acme/doc1.json")!=null);
+        assertTrue("Document /acme/doc2.json does not exist",docMgr.exists("/acme/doc2.json")!=null);
+        assertTrue("Contents for /acme/doc1.json not as expected.",
+                docMgr.read("/acme/doc1.json").next().getContent(new StringHandle()).toString().equals("{\"count\":1, \"total\":2}"));
+        assertTrue("Contents for /acme/doc2.json not as expected.",
+                docMgr.read("/acme/doc2.json").next().getContent(new StringHandle()).toString().equals("{\"count\":2, \"total\":3}"));
+    }
+
+    @Test
+    public void testWithJsonSchemaAndInvalidJsons() {
+        if (!Common.markLogicIsVersion11OrHigher()) {
+            return;
+        }
+        PlanBuilder.ModifyPlan plan = op
+                .fromDocDescriptors(
+                        op.docDescriptor(newWriteOp("/acme/doc1.json", mapper.createObjectNode().put("count", 1).put("total",2))),
+                        op.docDescriptor(newWriteOp("/acme/doc2.json", mapper.createObjectNode().put("count", -1).put("total",3))),
+                        op.docDescriptor(newWriteOp("/acme/doc3.json", mapper.createObjectNode().put("count", 2).put("total",13))),
+                        op.docDescriptor(newWriteOp("/acme/doc4.json", mapper.createObjectNode().put("count", -2).put("total",23))),
+                        op.docDescriptor(newWriteOp("/acme/doc5.json", mapper.createObjectNode().put("count", 3).put("total",33))),
+                        op.docDescriptor(newWriteOp("/acme/doc6.json", mapper.createObjectNode().put("count", 4).put("total",34))),
+                        op.docDescriptor(newWriteOp("/acme/doc7.json", mapper.createObjectNode().put("count", -5).put("total",38)))
+                )
+                .validateDoc(op.col("doc"),
+                        op.schemaDefinition("jsonSchema").withSchemaUri("/validateDoc/jsonSchema.json"));
+
+        verifyExportedPlanReturnsSameRowCount(plan);
+        Iterator<RowRecord> rows = rowManager.resultRows(plan).iterator();
+        while (rows.hasNext()){
+            RowRecord str = rows.next();
+            String uri = str.getString("uri");
+                expectedUris.add(uri);
+        }
+
+        rowManager.execute(plan.write());
+        DocumentManager docMgr = Common.client.newDocumentManager();
+        assertTrue("Document /acme/doc1.json does not exist",docMgr.exists("/acme/doc1.json")!=null);
+        assertTrue("Contents for /acme/doc1.json not as expected.",
+                docMgr.read("/acme/doc1.json").next().getContent(new StringHandle()).toString().equals("{\"count\":1, \"total\":2}"));
+        assertTrue("Document /acme/doc2.json should not exist",docMgr.exists("/acme/doc2.json") == null);
+        assertTrue("Document /acme/doc3.json does not exist",docMgr.exists("/acme/doc3.json") != null);
+        assertTrue("Contents for /acme/doc3.json not as expected.",
+                docMgr.read("/acme/doc3.json").next().getContent(new StringHandle()).toString().equals("{\"count\":2, \"total\":13}"));
+        assertTrue("Document /acme/doc4.json should not exist",docMgr.exists("/acme/doc4.json") == null);
+
+        assertTrue("Document /acme/doc5.json does not exist",docMgr.exists("/acme/doc5.json") != null);
+        assertTrue("Contents for /acme/doc5.json not as expected.",
+                docMgr.read("/acme/doc5.json").next().getContent(new StringHandle()).toString().equals("{\"count\":3, \"total\":33}"));
+
+        assertTrue("Document /acme/doc6.json does not exist",docMgr.exists("/acme/doc6.json") != null);
+        assertTrue("Contents for /acme/doc6.json not as expected.",
+                docMgr.read("/acme/doc6.json").next().getContent(new StringHandle()).toString().equals("{\"count\":4, \"total\":34}"));
+
+        assertTrue("Document /acme/doc7.json should not exist",docMgr.exists("/acme/doc7.json") == null);
+        assertTrue(expectedUris.size()==4);
     }
 }
