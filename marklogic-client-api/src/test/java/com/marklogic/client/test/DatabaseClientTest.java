97091827734b5c3483fa0ac14c372f330cd3b408
Rob Rudin <rudin@progress.com>
2024-12-20 08:44:48 -0500

    MLE-18458 Added support for try-with-resource

--- marklogic-client-api/src/test/java/com/marklogic/client/test/DatabaseClientTest.java
+++ marklogic-client-api/src/test/java/com/marklogic/client/test/DatabaseClientTest.java
@@ -3,14 +3,11 @@
  */
 package com.marklogic.client.test;
 
+import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.DatabaseClient.ConnectionResult;
 import com.marklogic.client.admin.QueryOptionsManager;
 import com.marklogic.client.alerting.RuleManager;
-import com.marklogic.client.document.BinaryDocumentManager;
-import com.marklogic.client.document.GenericDocumentManager;
-import com.marklogic.client.document.JSONDocumentManager;
-import com.marklogic.client.document.TextDocumentManager;
-import com.marklogic.client.document.XMLDocumentManager;
+import com.marklogic.client.document.*;
 import com.marklogic.client.eval.ServerEvaluationCall;
 import com.marklogic.client.pojo.PojoRepository;
 import com.marklogic.client.query.QueryManager;
@@ -19,105 +16,115 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class DatabaseClientTest {
-  @BeforeAll
-  public static void beforeClass() {
-    Common.connect();
-    Common.connectRestAdmin();
-  }
-  @AfterAll
-  public static void afterClass() {
-  }
+class DatabaseClientTest {
 
-  @Test
-  public void testNewDocument() {
-    GenericDocumentManager doc = Common.client.newDocumentManager();
-    assertNotNull( doc);
-  }
+	@BeforeAll
+	public static void beforeClass() {
+		Common.connect();
+		Common.connectRestAdmin();
+	}
 
-  @Test
-  public void testNewBinaryDocument() {
-    BinaryDocumentManager doc = Common.client.newBinaryDocumentManager();
-    assertNotNull( doc);
-  }
+	@AfterAll
+	public static void afterClass() {
+	}
 
-  @Test
-  public void testNewJSONDocument() {
-    JSONDocumentManager doc = Common.client.newJSONDocumentManager();
-    assertNotNull( doc);
-  }
+	@Test
+	void tryWithResource() {
+		try (DatabaseClient client = Common.newClient()) {
+			ConnectionResult result = client.checkConnection();
+			assertTrue(result.isConnected(), "This test is ensuring that a DatabaseClient, as of 7.1.0, can " +
+				"be used in a try-with-resources block without any error being thrown. We don't have a way of " +
+				"verifying that release() is actually called on the client though.");
+		}
+	}
 
-  @Test
-  public void testNewTextDocument() {
-    TextDocumentManager doc = Common.client.newTextDocumentManager();
-    assertNotNull( doc);
-  }
+	@Test
+	public void testNewDocument() {
+		GenericDocumentManager doc = Common.client.newDocumentManager();
+		assertNotNull(doc);
+	}
 
-  @Test
-  public void testNewXMLDocument() {
-    XMLDocumentManager doc = Common.client.newXMLDocumentManager();
-    assertNotNull( doc);
-  }
+	@Test
+	public void testNewBinaryDocument() {
+		BinaryDocumentManager doc = Common.client.newBinaryDocumentManager();
+		assertNotNull(doc);
+	}
 
-  @Test
-  public void testNewLogger() {
-    RequestLogger logger = Common.client.newLogger(System.out);
-    assertNotNull( logger);
-  }
+	@Test
+	public void testNewJSONDocument() {
+		JSONDocumentManager doc = Common.client.newJSONDocumentManager();
+		assertNotNull(doc);
+	}
 
-  @Test
-  public void testNewQueryManager() {
-    QueryManager mgr = Common.client.newQueryManager();
-    assertNotNull( mgr);
-  }
+	@Test
+	public void testNewTextDocument() {
+		TextDocumentManager doc = Common.client.newTextDocumentManager();
+		assertNotNull(doc);
+	}
 
-  @Test
-  public void testNewRuleManager() {
-    RuleManager mgr = Common.client.newRuleManager();
-    assertNotNull( mgr);
-  }
+	@Test
+	public void testNewXMLDocument() {
+		XMLDocumentManager doc = Common.client.newXMLDocumentManager();
+		assertNotNull(doc);
+	}
 
-  @Test
-  public void testNewPojoRepository() {
-    PojoRepository<City, Integer> mgr = Common.client.newPojoRepository(City.class, Integer.class);
-    assertNotNull( mgr);
-  }
+	@Test
+	public void testNewLogger() {
+		RequestLogger logger = Common.client.newLogger(System.out);
+		assertNotNull(logger);
+	}
 
-  @Test
-  public void testNewServerEvaluationCall() {
-    ServerEvaluationCall mgr = Common.client.newServerEval();
-    assertNotNull( mgr);
-  }
+	@Test
+	public void testNewQueryManager() {
+		QueryManager mgr = Common.client.newQueryManager();
+		assertNotNull(mgr);
+	}
 
-  @Test
-  public void testNewQueryOptionsManager() {
-    QueryOptionsManager mgr = Common.restAdminClient.newServerConfigManager().newQueryOptionsManager();
-    assertNotNull( mgr);
-  }
+	@Test
+	public void testNewRuleManager() {
+		RuleManager mgr = Common.client.newRuleManager();
+		assertNotNull(mgr);
+	}
 
-  @Test
-  public void testGetClientImplementationObject() {
-    Object impl = Common.client.getClientImplementation();
-    assertNotNull( impl);
-    assertTrue( impl instanceof okhttp3.OkHttpClient);
-  }
+	@Test
+	public void testNewPojoRepository() {
+		PojoRepository<City, Integer> mgr = Common.client.newPojoRepository(City.class, Integer.class);
+		assertNotNull(mgr);
+	}
 
-  @Test
-  public void testCheckConnectionWithValidUser() {
-    ConnectionResult connResult = Common.newClient().checkConnection();
-    assertTrue(connResult.isConnected());
-  }
+	@Test
+	public void testNewServerEvaluationCall() {
+		ServerEvaluationCall mgr = Common.client.newServerEval();
+		assertNotNull(mgr);
+	}
 
-  @Test
-  public void testCheckConnectionWithInvalidUser() {
-    ConnectionResult connResult = Common.newClientBuilder().withUsername("invalid").withPassword("invalid").build().checkConnection();
-    assertFalse(connResult.isConnected());
-    assertTrue(connResult.getStatusCode() == 401);
-    assertTrue(connResult.getErrorMessage().equalsIgnoreCase("Unauthorized"));
-  }
+	@Test
+	public void testNewQueryOptionsManager() {
+		QueryOptionsManager mgr = Common.restAdminClient.newServerConfigManager().newQueryOptionsManager();
+		assertNotNull(mgr);
+	}
+
+	@Test
+	public void testGetClientImplementationObject() {
+		Object impl = Common.client.getClientImplementation();
+		assertNotNull(impl);
+		assertTrue(impl instanceof okhttp3.OkHttpClient);
+	}
+
+	@Test
+	public void testCheckConnectionWithValidUser() {
+		ConnectionResult connResult = Common.newClient().checkConnection();
+		assertTrue(connResult.isConnected());
+	}
+
+	@Test
+	public void testCheckConnectionWithInvalidUser() {
+		ConnectionResult connResult = Common.newClientBuilder().withUsername("invalid").withPassword("invalid").build().checkConnection();
+		assertFalse(connResult.isConnected());
+		assertTrue(connResult.getStatusCode() == 401);
+		assertTrue(connResult.getErrorMessage().equalsIgnoreCase("Unauthorized"));
+	}
 
 }
