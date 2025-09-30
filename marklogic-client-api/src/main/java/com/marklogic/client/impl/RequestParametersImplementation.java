3a3bbd19f994ac4b1c401af8be130d5d76dcb32c
Rob Rudin <rudin@progress.com>
2025-09-30 12:03:56 -0400

    MLE-24504 Bumped Jakarta and removed javax.ws
    The javax.ws.rs-api dependency was only needed for a collection class that wasn't needed.
    
    Also forcing the usage of commons-lang3 as Black Duck mysteriously thinks an instance of 3.7 is being brought in as a direct dependency somehow.
    
    Also switched to latest nightly build for local testing, as Jenkins is using that too.

--- marklogic-client-api/src/main/java/com/marklogic/client/impl/RequestParametersImplementation.java
+++ marklogic-client-api/src/main/java/com/marklogic/client/impl/RequestParametersImplementation.java
@@ -3,22 +3,24 @@
  */
 package com.marklogic.client.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import javax.ws.rs.core.AbstractMultivaluedMap;
-import javax.ws.rs.core.MultivaluedMap;
 
 public abstract class RequestParametersImplementation {
-  private MultivaluedMap<String, String> map =
-    new AbstractMultivaluedMap<String,String>(new ConcurrentHashMap<>()) {};
 
-  protected RequestParametersImplementation() {
-    super();
-  }
+	// Prior to 8.0.0, this was a threadsafe map. However, that fact was not documented for a user. And in practice,
+	// it would not make sense for multiple threads to share a mutable instance of this, or of one of its subclasses.
+	// Additionally, the impl was from the 'javax.ws.rs:javax.ws.rs-api:2.1.1' dependency which wasn't used for
+	// anything else. So for 8.0.0, this is now simply a map that matches the intended usage of this class and its
+	// subclasses, which is to be used by a single thread.
+	private final Map<String, List<String>> map = new HashMap<>();
 
-  protected Map<String,List<String>> getMap() {
-    return map;
-  }
+	protected RequestParametersImplementation() {
+		super();
+	}
+
+	protected Map<String, List<String>> getMap() {
+		return map;
+	}
 }
