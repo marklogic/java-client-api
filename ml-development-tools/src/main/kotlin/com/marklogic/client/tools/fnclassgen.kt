7f0b7a8ebdf61b752489cd721336b057758dc7a4
Jonathan Miller <jonmille@progress.com>
2026-07-29 15:47:58 -0400

    MLE-31109 Bump kotlin plugin and remove kotlin.system.exitProcess
    Bump kotlin plugin from 2.4.0 to 2.4.10
    Removed imported instances of kotlin.system.exitProcess from com.squareup.okhttp3:okhttp:5.4.0 vulnerable dependency org.jetbrains.kotlin:kotlin-stdlib:2.2.21

--- ml-development-tools/src/main/kotlin/com/marklogic/client/tools/fnclassgen.kt
+++ ml-development-tools/src/main/kotlin/com/marklogic/client/tools/fnclassgen.kt
@@ -4,13 +4,11 @@
 package com.marklogic.client.tools
 
 import com.marklogic.client.tools.proxy.Generator
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
   if (args.size == 2) {
     Generator().serviceBundleToJava(args[0], args[1])
   } else {
-    System.err.println("usage: fnclassgen serviceDeclarationFile javaBaseDir")
-    exitProcess(-1)
+    throw IllegalArgumentException("usage: fnclassgen serviceDeclarationFile javaBaseDir")
   }
 }
--- ml-development-tools/src/main/kotlin/com/marklogic/client/tools/fnmodinit.kt
+++ ml-development-tools/src/main/kotlin/com/marklogic/client/tools/fnmodinit.kt
@@ -4,13 +4,11 @@
 package com.marklogic.client.tools
 
 import com.marklogic.client.tools.proxy.Generator
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
   if (args.size == 2) {
     Generator().endpointDeclToModStubImpl(args[0], args[1])
   } else {
-    System.err.println("usage: fnmodinit endpointDeclarationFile moduleExtension")
-    exitProcess(-1)
+    throw IllegalArgumentException("usage: fnmodinit endpointDeclarationFile moduleExtension")
   }
 }
